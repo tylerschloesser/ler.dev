42975ef9e579d7bb25e5976d31fca0903a88d26c
Tyler Schloesser <tschloes@amazon.com>
2021-03-07 09:05:05 -0800

    add cloudfront distribution

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,3 +1,5 @@
+import { Distribution, ViewerProtocolPolicy } from '@aws-cdk/aws-cloudfront'
+import { S3Origin } from '@aws-cdk/aws-cloudfront-origins'
 import { PublicHostedZone, TxtRecord } from '@aws-cdk/aws-route53'
 import { Bucket } from '@aws-cdk/aws-s3'
 import * as cdk from '@aws-cdk/core'
@@ -15,19 +17,32 @@ export class CdkStack extends cdk.Stack {
       zone,
     })
 
-    new Bucket(this, 'PublicAssetBucket', {
+    const publicAssetBucket = new Bucket(this, 'PublicAssetBucket', {
       bucketName: 'ler-dev-public-assets-prod',
       publicReadAccess: true,
+    })
+
+    new Distribution(this, 'PublicAssetDistribution', {
+      defaultBehavior: {
+        origin: new S3Origin(publicAssetBucket),
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
     })
   }
 
   protected allocateLogicalId(cfnElement: cdk.CfnElement): string {
     const tokens = cfnElement.logicalId.split('.')
     const id = tokens[1]
+    console.debug(tokens.join('.'))
 
     // for auto generated s3 bucket policies
     if (tokens[2] === 'Policy') {
-      return `${id}Policy`
+      return `${id}${tokens[2]}`
+    }
+
+    // for auto generated cloudfront distribution origins
+    if (tokens[2].startsWith('Origin')) {
+      return `${id}${tokens[2]}`
     }
 
     return id
