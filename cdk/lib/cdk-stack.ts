c774c5493e9ee5d0cf679e7cafdf6f480d48b02a
Tyler Schloesser <tschloes@amazon.com>
2021-03-07 08:52:24 -0800

    add public s3 bucket

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,4 +1,5 @@
 import { PublicHostedZone, TxtRecord } from '@aws-cdk/aws-route53'
+import { Bucket } from '@aws-cdk/aws-s3'
 import * as cdk from '@aws-cdk/core'
 
 export class CdkStack extends cdk.Stack {
@@ -13,9 +14,22 @@ export class CdkStack extends cdk.Stack {
       values: ['meow'],
       zone,
     })
+
+    new Bucket(this, 'PublicAssetBucket', {
+      bucketName: 'ler-dev-public-assets-prod',
+      publicReadAccess: true,
+    })
   }
 
   protected allocateLogicalId(cfnElement: cdk.CfnElement): string {
-    return cfnElement.logicalId.split('.')[1]
+    const tokens = cfnElement.logicalId.split('.')
+    const id = tokens[1]
+
+    // for auto generated s3 bucket policies
+    if (tokens[2] === 'Policy') {
+      return `${id}Policy`
+    }
+
+    return id
   }
 }
