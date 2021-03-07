b9f501ee19473a6cadf85af19bca12fa9842f42c
Tyler Schloesser <tschloes@amazon.com>
2021-03-07 08:11:05 -0800

    add test txt record

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,12 +1,17 @@
-import { PublicHostedZone } from '@aws-cdk/aws-route53';
+import { PublicHostedZone, TxtRecord } from '@aws-cdk/aws-route53';
 import * as cdk from '@aws-cdk/core';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new PublicHostedZone(this, 'PublicHostedZone', {
+    const zone = new PublicHostedZone(this, 'PublicHostedZone', {
       zoneName: 'ler.dev',
+    })
+
+    new TxtRecord(this, 'TestTxtRecord', {
+      values: ['meow'],
+      zone,
     })
   }
 
