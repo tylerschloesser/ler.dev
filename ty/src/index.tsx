bea445d30a67f583430a2e081d4197e68237d838
Tyler Schloesser <tylerschloesser@gmail.com>
2022-08-13 21:32:50 -0700

    refactor app

--- ty/src/index.tsx
+++ ty/src/index.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { App } from './app'
 
 const str: string = 'hi'
 console.log(str)
-
-const App = () => {
-  return <div>hello world</div>
-}
 
 const root = createRoot(document.getElementById('app')!)
 root.render(<App />)
