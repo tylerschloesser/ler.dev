667d340d8bbb308529b8bd369321f58500bb975a
Tyler Schloesser <tylerschloesser@gmail.com>
2022-09-02 09:30:40 -0700

    remove console

--- ty/src/index.tsx
+++ ty/src/index.tsx
@@ -2,8 +2,5 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './app'
 
-const str: string = 'hi'
-console.log(str)
-
 const root = createRoot(document.getElementById('app')!)
 root.render(<App />)
