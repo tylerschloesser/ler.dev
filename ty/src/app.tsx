acdb97326b9509d53bdcda05d8eff1a131d5721c
Tyler Schloesser <tylerschloesser@gmail.com>
2022-09-02 11:14:11 -0700

    add title

--- ty/src/app.tsx
+++ ty/src/app.tsx
@@ -28,12 +28,49 @@ function Layout() {
   )
 }
 
+const HomeContainer = styled.div`
+  position: relative;
+`
+
+const HomeBackground = styled.div`
+  position: relative;
+`
+
+const HomeTitle = styled.h1`
+  font-family: 'Space Mono', monospace;
+  font-weight: 700;
+  color: rgb(200, 200, 200, 0.9);
+  position: fixed;
+  font-size: min(8vh, 8vw);
+  bottom: 0;
+  right: 0;
+  pointer-events: none;
+  padding: min(4vh, 4vw);
+`
+
+const Period = styled.span`
+  margin: 0 calc(min(1vh, 1vw) * -1);
+`
+
+function Home() {
+  return (
+    <HomeContainer>
+      <HomeBackground>
+        <ThreeDemo />
+      </HomeBackground>
+      <HomeTitle>
+        ty<Period>.</Period>ler<Period>.</Period>dev
+      </HomeTitle>
+    </HomeContainer>
+  )
+}
+
 export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<ThreeDemo />} />
+          <Route index element={<Home />} />
         </Route>
       </Routes>
     </BrowserRouter>
