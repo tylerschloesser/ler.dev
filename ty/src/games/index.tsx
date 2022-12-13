d68510f816872800b0d9365f0ab9c73e2b5c8926
Tyler Schloesser <tylerschloesser@gmail.com>
2022-12-13 07:43:29 -0800

    add init canvas placeholder

--- ty/src/games/index.tsx
+++ ty/src/games/index.tsx
@@ -1,5 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+function initCanvas(canvas: HTMLCanvasElement) {
+  console.log('todo init canvas')
+}
 
 export function Games() {
-  return <>games</>
+  const [canvas, setCanvas] = useState<HTMLCanvasElement | null>()
+  useEffect(() => {
+    canvas && initCanvas(canvas)
+  }, [canvas])
+  return (
+    <div>
+      <canvas
+        ref={setCanvas}
+        width={window.innerWidth}
+        height={window.innerHeight}
+      />
+    </div>
+  )
 }
