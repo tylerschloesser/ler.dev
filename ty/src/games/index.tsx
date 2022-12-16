a3f9f01a14c7a94757c0b3c8cf13dddef0927c9a
Tyler Schloesser <tylerschloesser@gmail.com>
2022-12-16 11:16:49 -0800

    refactor vec2

--- ty/src/games/index.tsx
+++ ty/src/games/index.tsx
@@ -1,41 +1,8 @@
 import React, { useEffect, useState } from 'react'
+import { Vec2 } from './vec2'
 
 // IDEAS
 // two balls, input controls both balls
-
-class Vec2 {
-  x: number
-  y: number
-
-  constructor(x: number = 0, y: number = 0) {
-    this.x = x
-    this.y = y
-  }
-
-  add(v: Vec2): Vec2 {
-    return new Vec2(this.x + v.x, this.y + v.y)
-  }
-
-  sub(v: Vec2): Vec2 {
-    return new Vec2(this.x - v.x, this.y - v.y)
-  }
-
-  mul(s: number): Vec2 {
-    return new Vec2(this.x * s, this.y * s)
-  }
-
-  div(s: number): Vec2 {
-    return new Vec2(this.x / s, this.y / s)
-  }
-
-  length(): number {
-    return Math.sqrt(this.x * this.x + this.y * this.y)
-  }
-
-  toString(): string {
-    return `[${Math.round(this.x)},${Math.round(this.y)}]`
-  }
-}
 
 interface Viewport {
   w: number
---
+++ ty/src/games/vec2.ts
@@ -0,0 +1,33 @@
+export class Vec2 {
+  x: number
+  y: number
+
+  constructor(x: number = 0, y: number = 0) {
+    this.x = x
+    this.y = y
+  }
+
+  add(v: Vec2): Vec2 {
+    return new Vec2(this.x + v.x, this.y + v.y)
+  }
+
+  sub(v: Vec2): Vec2 {
+    return new Vec2(this.x - v.x, this.y - v.y)
+  }
+
+  mul(s: number): Vec2 {
+    return new Vec2(this.x * s, this.y * s)
+  }
+
+  div(s: number): Vec2 {
+    return new Vec2(this.x / s, this.y / s)
+  }
+
+  length(): number {
+    return Math.sqrt(this.x * this.x + this.y * this.y)
+  }
+
+  toString(): string {
+    return `[${Math.round(this.x)},${Math.round(this.y)}]`
+  }
+}
