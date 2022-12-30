c1324a7470470986927498642b4ff4cec82895ac
Tyler Schloesser <tylerschloesser@gmail.com>
2022-12-30 15:06:53 -0800

    add cal container

--- ty/src/cal/index.tsx
+++ ty/src/cal/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+`
 
 export function Cal() {
-  return <>Cal</>
+  return <Container>Cal</Container>
 }
