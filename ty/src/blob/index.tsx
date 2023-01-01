56b9e5493100aabbe44b202a42323149ff94b440
Tyler Schloesser <tylerschloesser@gmail.com>
2022-12-31 18:05:52 -0800

    add circle

--- ty/src/blob/index.tsx
+++ ty/src/blob/index.tsx
@@ -8,6 +8,20 @@ function init(canvas: HTMLCanvasElement) {
     canvas.height = rect.height
   }
   const context = canvas.getContext('2d')!
+
+  {
+    const x = canvas.width / 2
+    const y = canvas.height / 2
+    context.fillStyle = 'hsl(0, 0%, 80%)'
+    context.arc(
+      x,
+      y,
+      Math.min(canvas.width, canvas.height) * 0.1,
+      0,
+      Math.PI * 2,
+    )
+    context.fill()
+  }
 }
 
 const Canvas = styled.canvas`
