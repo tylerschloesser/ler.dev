92a5f22accdaf037a47d443e2566fd067579a9e8
Tyler Schloesser <tylerschloesser@gmail.com>
2022-12-31 17:42:32 -0800

    add basic grid

--- ty/src/cal/index.tsx
+++ ty/src/cal/index.tsx
@@ -1,12 +1,31 @@
+import { times } from 'lodash'
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
+
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  grid-template-rows: repeat(6, 1fr);
+
+  color: white;
+`
+
+const Cell = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
 `
 
 export function Cal() {
   const [container, setContainer] = useState<HTMLDivElement | null>()
-  return <Container ref={setContainer}>Cal</Container>
+
+  return (
+    <Container ref={setContainer}>
+      {times(14).map((i) => (
+        <Cell key={i}>{i + 1}</Cell>
+      ))}
+    </Container>
+  )
 }
