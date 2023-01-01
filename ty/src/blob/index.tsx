b67f462f610aa4ca795b9fdf7e7493ca94fe91e1
Tyler Schloesser <tylerschloesser@gmail.com>
2022-12-31 17:50:30 -0800

    init blob canvas

--- ty/src/blob/index.tsx
+++ ty/src/blob/index.tsx
@@ -1,5 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+
+function init(canvas: HTMLCanvasElement) {
+  {
+    const rect = canvas.getBoundingClientRect()
+    canvas.width = rect.width
+    canvas.height = rect.height
+  }
+  const context = canvas.getContext('2d')!
+}
+
+const Canvas = styled.canvas`
+  display: block;
+  height: 100%;
+  width: 100%;
+`
 
 export function Blob() {
-  return <>blob</>
+  const [canvas, setCanvas] = useState<HTMLCanvasElement | null>()
+  useEffect(() => {
+    canvas && init(canvas)
+  }, [canvas])
+  return <Canvas ref={setCanvas} />
 }
