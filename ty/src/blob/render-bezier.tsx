e35fbbff2ac79e59a7c7b0c3a1f1052f7566860e
Tyler Schloesser <tylerschloesser@gmail.com>
2023-01-02 09:17:35 -0800

    refactor bezier to use vec2

--- ty/src/blob/render-bezier.tsx
+++ ty/src/blob/render-bezier.tsx
@@ -1,4 +1,5 @@
 import { createNoise3D } from 'simplex-noise'
+import { Vec2 } from '../common/vec2'
 import { RenderFn } from './config'
 
 const noise = createNoise3D()
@@ -7,36 +8,28 @@ export const renderBezier: RenderFn = (canvas, context, config, timestamp) => {
   context.clearRect(0, 0, canvas.width, canvas.height)
   context.fillStyle = 'hsl(0, 0%, 80%)'
 
-  const translate = {
-    x: canvas.width / 2,
-    y: canvas.height / 2,
-  }
+  const translate = new Vec2(canvas.width / 2, canvas.height / 2)
 
   context.beginPath()
   context.moveTo(translate.x + 0, translate.y + 0)
 
-  let previous = {
-    x: translate.x + 0,
-    y: translate.y + 0,
-  }
+  let previous = new Vec2(translate.x + 0, translate.y + 0)
 
   const { parts, xScale, yScale, zScale } = config
 
   for (let i = 0; i <= parts; i++) {
     const theta = ((Math.PI * 2) / parts) * i
 
-    let x = Math.sin(theta)
-    let y = Math.cos(theta)
+    let p = new Vec2(Math.sin(theta), Math.cos(theta))
 
     let radius = Math.min(canvas.width, canvas.height) * 0.15
     radius +=
-      ((noise(x * xScale, y * yScale, timestamp * zScale) + 1) / 2) *
+      ((noise(p.x * xScale, p.y * yScale, timestamp * zScale) + 1) / 2) *
       (radius / 1)
 
-    x *= radius
-    y *= radius
+    p = p.mul(radius)
 
-    context.bezierCurveTo(previous.x, previous.y, x, y, x, y)
+    context.bezierCurveTo(previous.x, previous.y, p.x, p.y, p.x, p.y)
   }
   context.fill()
   context.closePath()
