d5bf58acd5596072e8ecb1e900bf0ae6794ac6aa
Tyler Schloesser <tylerschloesser@gmail.com>
2023-01-01 17:20:46 -0800

    add parts slider

--- ty/src/blob/index.tsx
+++ ty/src/blob/index.tsx
@@ -1,21 +1,22 @@
 import { curry } from 'lodash/fp'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { createNoise3D } from 'simplex-noise'
 import styled from 'styled-components'
 
 const noise = createNoise3D()
 
-const config = {
-  parts: 600,
-  xScale: 10.9,
-  yScale: 10.9,
-  zScale: 0.00025,
+interface Config {
+  parts: number
+  xScale: number
+  yScale: number
+  zScale: number
 }
 
 const render = curry(
   (
     canvas: HTMLCanvasElement,
     context: CanvasRenderingContext2D,
+    config: Config,
     timestamp: number,
   ) => {
     context.clearRect(0, 0, canvas.width, canvas.height)
@@ -52,18 +53,18 @@ const render = curry(
       context.closePath()
     }
 
-    window.requestAnimationFrame(render(canvas, context))
+    window.requestAnimationFrame(render(canvas, context, config))
   },
 )
 
-function init(canvas: HTMLCanvasElement) {
+function init(canvas: HTMLCanvasElement, config: Config) {
   {
     const rect = canvas.getBoundingClientRect()
     canvas.width = rect.width
     canvas.height = rect.height
   }
   const context = canvas.getContext('2d')!
-  window.requestAnimationFrame(render(canvas, context))
+  window.requestAnimationFrame(render(canvas, context, config))
 }
 
 const Canvas = styled.canvas`
@@ -72,10 +73,47 @@ const Canvas = styled.canvas`
   width: 100%;
 `
 
+const Controls = styled.div`
+  position: fixed;
+`
+
 export function Blob() {
   const [canvas, setCanvas] = useState<HTMLCanvasElement | null>()
+  const [config, setConfig] = useState<Config>({
+    parts: 600,
+    xScale: 10.9,
+    yScale: 10.9,
+    zScale: 0.00025,
+  })
+  const configRef = useRef(config)
+
   useEffect(() => {
-    canvas && init(canvas)
+    Object.assign(configRef.current, config)
+  }, [config])
+
+  useEffect(() => {
+    canvas && init(canvas, configRef.current)
   }, [canvas])
-  return <Canvas ref={setCanvas} />
+
+  return (
+    <>
+      <Controls>
+        <label>
+          parts:
+          <input
+            type="range"
+            min={2}
+            max={1000}
+            value={config.parts}
+            onChange={(e) =>
+              setConfig({
+                parts: parseInt(e.target.value),
+              } as Config)
+            }
+          />
+        </label>
+      </Controls>
+      <Canvas ref={setCanvas} />
+    </>
+  )
 }
