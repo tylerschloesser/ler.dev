f407f70d0bf759d5b592edf3b5fc6551dbf462e1
Tyler Schloesser <tylerschloesser@gmail.com>
2023-01-08 16:50:46 -0800

    position images

--- ty/src/menu/demo.tsx
+++ ty/src/menu/demo.tsx
@@ -1,15 +1,37 @@
 import { shuffle, times } from 'lodash'
 import React from 'react'
+import styled from 'styled-components'
+
+const ImageList = styled.ol`
+  display: flex;
+  flex-direction: column;
+  width: 50vw;
+`
+
+const ImageListItem = styled.li`
+  box-sizing: border-box;
+  width: 100%;
+  padding: 1rem;
+
+  &:not(:last-child) {
+    padding-bottom: 0;
+  }
+`
+
+const Image = styled.img`
+  width: 100%;
+`
 
 export function Demo() {
   const images = shuffle(times(5)).map((i) => (
-    <img key={i} src={`menu-demo-${i + 1}.jpg`} width="200px" />
+    <ImageListItem>
+      <Image key={i} src={`menu-demo-${i + 1}.jpg`} />
+    </ImageListItem>
   ))
 
   return (
     <>
-      demo
-      {images}
+      <ImageList>{images}</ImageList>
     </>
   )
 }
