645a5f7bdfa9569684453a0ea46f9c3aeb69fa42
Tyler Schloesser <tylerschloesser@gmail.com>
2023-01-09 11:20:42 -0800

    add render to tetris

--- ty/src/tetris/index.tsx
+++ ty/src/tetris/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Engine, InitFn, RenderFn } from '../common/engine'
 
 interface State {
   board: boolean[][]
@@ -7,19 +8,23 @@ interface State {
 const NUM_ROWS = 20
 const NUM_COLS = 10
 
-function init() {
+const state: State = (() => {
   const board: boolean[][] = new Array(NUM_ROWS).fill(null)
   for (let i = 0; i < board.length; i++) {
     board[i] = new Array(NUM_COLS).fill(false)
   }
 
-  const state: State = {
+  return {
     board,
   }
+})()
+
+const init: InitFn = () => {
   console.log(state)
 }
 
+const render: RenderFn = () => {}
+
 export function Tetris() {
-  init()
-  return <>todo</>
+  return <Engine init={init} render={render} />
 }
