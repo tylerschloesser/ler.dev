f2c47811ff3645644e714dbad431d388f1f5bd5c
Tyler Schloesser <tylerschloesser@gmail.com>
2023-01-09 14:40:58 -0800

    center grid

--- ty/src/tetris/render.ts
+++ ty/src/tetris/render.ts
@@ -6,12 +6,22 @@ export const render: RenderFn = ({ context, viewport }) => {
 
   context.clearRect(0, 0, viewport.w, viewport.h)
 
-  context.translate(10, 10)
   context.strokeStyle = 'white'
   context.fillStyle = 'white'
   context.beginPath()
 
-  const size = Math.min(viewport.w / NUM_COLS, viewport.h / NUM_ROWS)
+  const padding = Math.min(viewport.w, viewport.h) * 0.05
+  const size = Math.min(
+    (viewport.w - padding * 2) / NUM_COLS,
+    (viewport.h - padding * 2) / NUM_ROWS,
+  )
+
+  context.translate(padding, padding)
+
+  context.translate(
+    Math.max((viewport.w - size * NUM_COLS - padding * 2) / 2, 0),
+    Math.max((viewport.h - size * NUM_ROWS - padding * 2) / 2, 0),
+  )
 
   for (let row = 0; row < board.length; row++) {
     for (let col = 0; col < board[0].length; col++) {
