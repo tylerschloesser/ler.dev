ca57dfc8b3062836081cb78d2d994b6a9b537eb4
Tyler Schloesser <tylerschloesser@gmail.com>
2023-01-08 16:42:35 -0800

    add more menu images

--- ty/src/menu/demo.tsx
+++ ty/src/menu/demo.tsx
@@ -1,10 +1,15 @@
+import { shuffle, times } from 'lodash'
 import React from 'react'
 
 export function Demo() {
+  const images = shuffle(times(5)).map((i) => (
+    <img key={i} src={`menu-demo-${i + 1}.jpg`} width="200px" />
+  ))
+
   return (
     <>
       demo
-      <img src="menu-demo-1.jpg" width="200px" />
+      {images}
     </>
   )
 }
