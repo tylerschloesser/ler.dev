1391f1c29bbd37f6a532690400c65393a1ee180a
Tyler Schloesser <tylerschloesser@gmail.com>
2023-01-09 14:33:26 -0800

    refactor initial board

--- ty/src/tetris/state.ts
+++ ty/src/tetris/state.ts
@@ -5,13 +5,16 @@ export interface State {
 export const NUM_ROWS = 20
 export const NUM_COLS = 10
 
-export const state: State = (() => {
+function createEmptyBoard() {
   const board: boolean[][] = new Array(NUM_ROWS).fill(null)
   for (let i = 0; i < board.length; i++) {
     board[i] = new Array(NUM_COLS).fill(false)
   }
+  return board
+}
 
+export const state: State = (() => {
   return {
-    board,
+    board: createEmptyBoard(),
   }
 })()
