8e17dc049e2cf400335b31e62f82c01c28ceb499
Tyler Schloesser <tylerschloesser@gmail.com>
2023-01-31 10:58:56 -0800

    stroke rect

--- ty/src/draw/index.tsx
+++ ty/src/draw/index.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
+import { Engine, InitFn, RenderFn } from '../common/engine'
+
+const init: InitFn = () => {}
+
+const render: RenderFn = ({ context, viewport }) => {
+  context.clearRect(0, 0, viewport.w, viewport.h)
+
+  context.strokeStyle = 'green'
+  context.strokeRect(0, 0, 100, 100)
+}
 
 export function Draw() {
-  return <>hi</>
+  return <Engine init={init} render={render} />
 }
