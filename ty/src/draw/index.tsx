fe3030ef27f892609e9845fe3168e06f11267fa2
Tyler Schloesser <tylerschloesser@gmail.com>
2023-01-30 20:02:57 -0800

    render circle pointer

--- ty/src/draw/index.tsx
+++ ty/src/draw/index.tsx
@@ -1,16 +1,28 @@
 import React from 'react'
 import { Engine, InitFn, RenderFn } from '../common/engine'
+import { Vec2 } from '../common/vec2'
 
-const render: RenderFn = () => {}
+let pointer: Vec2 | null = null
 
 const init: InitFn = ({ canvas, signal }) => {
   canvas.addEventListener(
     'pointermove',
     (e) => {
-      console.log(e.clientX, e.clientY)
+      pointer = new Vec2(e.clientX, e.clientY)
     },
     { signal },
   )
+}
+
+const render: RenderFn = ({ context, viewport }) => {
+  context.clearRect(0, 0, viewport.w, viewport.h)
+  if (pointer) {
+    const { x, y } = pointer
+    context.beginPath()
+    context.strokeStyle = 'white'
+    context.arc(x, y, 20, 0, Math.PI * 2)
+    context.stroke()
+  }
 }
 
 export function Draw() {
