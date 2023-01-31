cd4b733092876cd908a55ffb1be3a2d8d9f5bb3a
Tyler Schloesser <tylerschloesser@gmail.com>
2023-01-30 20:21:08 -0800

    add red static

--- ty/src/draw/index.tsx
+++ ty/src/draw/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Engine, InitFn, RenderFn } from '../common/engine'
 import { Vec2 } from '../common/vec2'
+import * as state from './state'
 
 let pointer: Vec2 | null = null
 
@@ -12,10 +13,29 @@ const init: InitFn = ({ canvas, signal }) => {
     },
     { signal },
   )
+
+  console.log(state.debug.grid)
 }
 
 const render: RenderFn = ({ context, viewport }) => {
   context.clearRect(0, 0, viewport.w, viewport.h)
+
+  for (let col = 0; col < state.size().cols; col++) {
+    for (let row = 0; row < state.size().rows; row++) {
+      const color = state.get(col, row)
+      if (!color) {
+        continue
+      }
+      context.fillStyle = color
+      const size = 4
+      const x = size * col,
+        y = size * row,
+        w = size,
+        h = size
+      context.fillRect(x, y, w, h)
+    }
+  }
+
   if (pointer) {
     const { x, y } = pointer
     context.beginPath()
--- ty/src/draw/state.ts
+++ ty/src/draw/state.ts
@@ -4,9 +4,11 @@ type Grid = Color[][]
 const GRID_COLS = 100
 const GRID_ROWS = 100
 
-const grid: Grid = new Array(GRID_COLS)
-  .fill(null)
-  .map(() => new Array(GRID_ROWS).fill(null))
+const grid: Grid = new Array(GRID_COLS).fill(null).map(() =>
+  new Array(GRID_ROWS).fill(null).map(() => {
+    return `hsl(0, 80%, ${Math.floor(Math.random() * 80)}%)`
+  }),
+)
 
 function set(col: number, row: number, color: Color) {
   grid[col][row] = color
@@ -21,6 +23,10 @@ function size() {
     cols: grid.length,
     rows: grid[0].length,
   }
+}
+
+export const debug = {
+  grid,
 }
 
 export { set, get, size }
