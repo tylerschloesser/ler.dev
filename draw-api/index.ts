5e00c347e410c2853bb0e3eff77bbc62130d4b37
Tyler Schloesser <tylerschloesser@gmail.com>
2023-02-01 15:45:08 -0800

    log connection id

--- draw-api/index.ts
+++ draw-api/index.ts
@@ -1,7 +1,7 @@
-import { APIGatewayProxyResult } from 'aws-lambda'
+import { APIGatewayProxyWebsocketHandlerV2 } from 'aws-lambda'
 
-export const handler = async (): Promise<APIGatewayProxyResult> => {
-  console.log('connected!')
+export const handler: APIGatewayProxyWebsocketHandlerV2 = async (event) => {
+  console.log('connectionId:', event.requestContext.connectionId)
   return {
     statusCode: 200,
     body: JSON.stringify({
