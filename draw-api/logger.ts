b74e1f25b342f16980e6c00059b9439a75b90490
Tyler Schloesser <tylerschloesser@gmail.com>
2023-02-07 10:10:47 -0800

    cleanup logging

--- draw-api/logger.ts
+++ draw-api/logger.ts
@@ -4,9 +4,7 @@ import winston, { format } from 'winston'
 // So that multi-line logs are not split into multiple cloudwatch logs
 // https://github.com/winstonjs/winston/issues/1895#issuecomment-1216079573
 const replaceNewlinesWithCarriageReturns = format((info, _opts) => {
-  console.log('replacing?')
   info[MESSAGE as any] = info[MESSAGE as any].replace(/\n/g, '\r')
-  console.log(MESSAGE, JSON.stringify(info))
   return info
 })
 
@@ -14,10 +12,6 @@ export const logger = winston.createLogger({
   level: 'debug',
   transports: [
     new winston.transports.Console({
-      // log(info, callback) {
-      //   console.log((MESSAGE as any)[info])
-      //   callback()
-      // },
       format: format.combine(
         format.prettyPrint({
           colorize: true,
