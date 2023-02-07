f482beb3a6096bcd9c88b7f0317c70e01f11290f
Tyler Schloesser <tylerschloesser@gmail.com>
2023-02-07 09:37:06 -0800

    pretty print logger

--- draw-api/logger.ts
+++ draw-api/logger.ts
@@ -2,10 +2,11 @@ import winston from 'winston'
 
 export const logger = winston.createLogger({
   level: 'debug',
-  format: winston.format.json(),
   transports: [
     new winston.transports.Console({
-      format: winston.format.simple(),
+      format: winston.format.prettyPrint({
+        colorize: true,
+      }),
     }),
   ],
 })
