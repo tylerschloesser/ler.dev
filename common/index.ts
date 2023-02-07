d07168d912c82898faebf2b21fe8669df070a190
Tyler Schloesser <tylerschloesser@gmail.com>
2023-02-06 16:39:55 -0800

    remove io-ts

--- common/index.ts
+++ common/index.ts
@@ -1,22 +1,4 @@
-import * as t from 'io-ts'
 import { z } from 'zod'
-
-export const Cell = t.type({
-  x: t.number,
-  y: t.number,
-  color: t.string,
-})
-
-export const DrawPayload = t.type({
-  cells: t.array(Cell),
-})
-
-export const DrawMessage = t.type({
-  action: t.literal('draw'),
-  payload: DrawPayload,
-})
-
-export const Message = t.exact(DrawMessage)
 
 export const DrawRequest = z.object({
   action: z.literal('draw'),
