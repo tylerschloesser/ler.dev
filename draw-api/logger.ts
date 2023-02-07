ca513876cb1cfa2a5c391eb4040078e1e0b7c39f
Tyler Schloesser <tylerschloesser@gmail.com>
2023-02-07 09:54:47 -0800

    fix cloudwatch logs

--- draw-api/logger.ts
+++ draw-api/logger.ts
@@ -1,12 +1,24 @@
-import winston from 'winston'
+import winston, { format } from 'winston'
+
+const MESSAGE = Symbol.for('message') as any
+
+// So that multi-line logs are not split into multiple cloudwatch logs
+// https://github.com/winstonjs/winston/issues/1895#issuecomment-1216079573
+const replaceNewlinesWithCarriageReturns = format((info, _opts) => {
+  info[MESSAGE] = info[MESSAGE].replace(/\n/g, '\r')
+  return info
+})
 
 export const logger = winston.createLogger({
   level: 'debug',
   transports: [
     new winston.transports.Console({
-      format: winston.format.prettyPrint({
-        colorize: true,
-      }),
+      format: format.combine(
+        format.prettyPrint({
+          colorize: true,
+        }),
+        replaceNewlinesWithCarriageReturns(),
+      ),
     }),
   ],
 })
