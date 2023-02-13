a5c03d3abedfe2744eb2ae1462cb97e9e4b1ee11
Tyler Schloesser <tylerschloesser@gmail.com>
2023-02-13 11:11:10 -0800

    add more flags

--- ty/src/conquest/init.ts
+++ ty/src/conquest/init.ts
@@ -6,7 +6,17 @@ export const init: InitFn = ({ canvas, signal }) => {
   state.world.flags.push({
     color: 'red',
     r: 10,
-    p: new Vec2(0, 0),
+    p: new Vec2(20, 20),
+  })
+  state.world.flags.push({
+    color: 'green',
+    r: 20,
+    p: new Vec2(100, 50),
+  })
+  state.world.flags.push({
+    color: 'blue',
+    r: 60,
+    p: new Vec2(300, 200),
   })
 
   canvas.addEventListener(
