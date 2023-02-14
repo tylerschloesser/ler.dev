3364cd5fe156c292d7a1ca1b2c86d542c15e057d
Tyler Schloesser <tylerschloesser@gmail.com>
2023-02-13 20:41:55 -0800

    refactor input

--- ty/src/conquest/init.ts
+++ ty/src/conquest/init.ts
@@ -1,7 +1,6 @@
 import { InitFn } from '../common/engine'
-import { Vec2 } from '../common/vec2'
 import { generateFlags } from './generate-flags'
-import { state } from './state'
+import { handlePointer, handleWheel } from './input'
 
 export const init: InitFn = ({ canvas, signal, updateConfig }) => {
   updateConfig((prev) => ({
@@ -23,36 +22,7 @@ export const init: InitFn = ({ canvas, signal, updateConfig }) => {
     }
   })
 
-  const handlePointer = (e: PointerEvent) => {
-    const { clientX: x, clientY: y } = e
-    state.pointer = new Vec2(x, y)
-
-    if (e.pressure) {
-      if (state.drag?.a) {
-        state.drag.b = new Vec2(x, y)
-      } else {
-        state.drag = {
-          a: new Vec2(x, y),
-          b: null,
-        }
-      }
-    } else if (e.type === 'pointerup') {
-      if (state.drag?.b) {
-        state.ball.v = state.drag.b.sub(state.drag.a).mul(-1)
-      }
-      state.drag = null
-    }
-  }
-
   canvas.addEventListener('pointermove', handlePointer, { signal })
   canvas.addEventListener('pointerup', handlePointer, { signal })
-
-  canvas.addEventListener(
-    'wheel',
-    (e) => {
-      state.camera.zoom += e.deltaY * 0.001 * -1
-      state.camera.zoom = Math.max(state.camera.zoom, 1)
-    },
-    { signal, passive: true },
-  )
+  canvas.addEventListener('wheel', handleWheel, { signal, passive: true })
 }
---
+++ ty/src/conquest/input.ts
@@ -0,0 +1,28 @@
+import { Vec2 } from '../common/vec2'
+import { state } from './state'
+
+export const handlePointer = (e: PointerEvent) => {
+  const { clientX: x, clientY: y } = e
+  state.pointer = new Vec2(x, y)
+
+  if (e.pressure) {
+    if (state.drag?.a) {
+      state.drag.b = new Vec2(x, y)
+    } else {
+      state.drag = {
+        a: new Vec2(x, y),
+        b: null,
+      }
+    }
+  } else if (e.type === 'pointerup') {
+    if (state.drag?.b) {
+      state.ball.v = state.drag.b.sub(state.drag.a).mul(-1)
+    }
+    state.drag = null
+  }
+}
+
+export const handleWheel = (e: WheelEvent) => {
+  state.camera.zoom += e.deltaY * 0.001 * -1
+  state.camera.zoom = Math.max(state.camera.zoom, 1)
+}
