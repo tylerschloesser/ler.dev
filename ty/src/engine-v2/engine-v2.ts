a83b6fb1276646b92d8a10950e6487e3f3b504d8
Tyler Schloesser <tylerschloesser@gmail.com>
2023-02-16 10:46:56 -0800

    refactor abort controller

--- ty/src/engine-v2/engine-v2.ts
+++ ty/src/engine-v2/engine-v2.ts
@@ -1,25 +1,33 @@
+import { InitFn, RenderFn } from '../common/engine'
+
 type Milliseconds = number
 
 export interface ConstructorArgs {
   container: HTMLElement
+  init: InitFn
+  render: RenderFn
 }
 
 export class EngineV2 {
   readonly container: HTMLElement
   readonly canvas: HTMLCanvasElement
+  readonly controller: AbortController
 
   constructor({ container }: ConstructorArgs) {
     this.container = container
     this.canvas = document.createElement('canvas')
     this.container.appendChild(this.canvas)
+
+    this.controller = new AbortController()
+
     window.requestAnimationFrame(this.handleFrame.bind(this))
   }
 
   private handleFrame(timestamp: Milliseconds) {
+    if (this.controller.signal.aborted) {
+      return
+    }
+    console.log('handle frame')
     window.requestAnimationFrame(this.handleFrame.bind(this))
-  }
-
-  cleanup() {
-    this.canvas.remove()
   }
 }
--- ty/src/jump/jump.tsx
+++ ty/src/jump/jump.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import { EngineV2 } from '../engine-v2'
+import { init } from './init'
+import { render } from './render'
 
 export function Jump() {
   const [container, setContainer] = useState<HTMLElement | null>(null)
   useEffect(() => {
     if (!container) return
-    const engine = new EngineV2({ container })
+    const engine = new EngineV2({ container, init, render })
     return () => {
-      engine.cleanup()
+      engine.controller.abort()
     }
   }, [container])
   return <div ref={setContainer} />
