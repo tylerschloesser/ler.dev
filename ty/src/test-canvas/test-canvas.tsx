6391ce28e57a3d21459f44bef0b37de86e6be254
Tyler Schloesser <tylerschloesser@gmail.com>
2023-02-16 08:58:26 -0800

    implement test canvas

--- ty/src/test-canvas/test-canvas.tsx
+++ ty/src/test-canvas/test-canvas.tsx
@@ -1,5 +1,56 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+
+interface Perf {
+  frames: number
+  last: number
+}
 
 export function TestCanvas() {
-  return <>todo</>
+  const [fps, setFps] = useState(0)
+  const perf = useRef<Perf>({ frames: 0, last: 0 })
+  const canvas = useRef<HTMLCanvasElement>(null)
+
+  useEffect(() => {
+    let stop = false
+
+    function handleFrame(timestamp: number) {
+      if (!canvas.current) {
+        if (!stop) requestAnimationFrame(handleFrame)
+        return
+      }
+
+      if (
+        Math.floor(timestamp / 1000) !== Math.floor(perf.current.last / 1000)
+      ) {
+        setFps(perf.current.frames)
+        perf.current = {
+          frames: 1,
+          last: Math.floor(timestamp / 1000) * 1000,
+        }
+      } else {
+        perf.current.frames++
+      }
+
+      const context = canvas.current.getContext('2d')!
+      context.clearRect(0, 0, canvas.current.width, canvas.current.height)
+
+      context.strokeStyle = 'white'
+      context.lineWidth = 2
+      context.strokeRect(10, 10, 20, 20)
+
+      if (!stop) requestAnimationFrame(handleFrame)
+    }
+    requestAnimationFrame(handleFrame)
+
+    return () => {
+      stop = true
+    }
+  }, [])
+
+  return (
+    <div>
+      FPS: {fps}
+      <canvas ref={canvas}></canvas>
+    </div>
+  )
 }
