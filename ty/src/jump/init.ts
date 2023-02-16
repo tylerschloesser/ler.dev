2a684549ed30a5e99d7afadb2fbdd0fe7443793a
Tyler Schloesser <tylerschloesser@gmail.com>
2023-02-16 08:42:41 -0800

    show fps

--- ty/src/jump/init.ts
+++ ty/src/jump/init.ts
@@ -2,9 +2,15 @@ import { InitFn } from '../common/engine'
 import { handlePointer } from './input'
 import { state } from './state'
 
-export const init: InitFn = ({ canvas, signal, viewport }) => {
+export const init: InitFn = ({ canvas, signal, viewport, updateConfig }) => {
   // TODO update viewport
   state.viewport = viewport
+
+  updateConfig((prev) => ({
+    ...prev,
+    showFps: true,
+    debugFontColor: 'white',
+  }))
 
   canvas.addEventListener('pointerdown', handlePointer, { signal })
   canvas.addEventListener('pointermove', handlePointer, { signal })
