7701f7a4f12efea90ad797ee5a7df058d952d164
Tyler Schloesser <tylerschloesser@gmail.com>
2023-11-10 17:35:31 -0800

    render circle

--- packages/demo-gear/src/index.tsx
+++ packages/demo-gear/src/index.tsx
@@ -3,11 +3,27 @@ import { InitCanvasFn } from './types.js'
 import { useCanvas } from './use-canvas.js'
 
 import styles from './index.module.scss'
+import invariant from 'tiny-invariant'
 
 const initCanvas: InitCanvasFn = (canvas) => {
   const rect = canvas.getBoundingClientRect()
   canvas.width = rect.width
   canvas.height = rect.height
+
+  const context = canvas.getContext('2d')
+  invariant(context)
+  context.fillStyle = 'grey'
+  context.fillRect(0, 0, canvas.width, canvas.height)
+
+  context.fillStyle = 'black'
+  context.arc(
+    canvas.width / 2,
+    canvas.height / 2,
+    Math.min(canvas.width, canvas.height) / 4,
+    0,
+    Math.PI * 2,
+  )
+  context.fill()
 }
 
 const DemoGear = () => {
