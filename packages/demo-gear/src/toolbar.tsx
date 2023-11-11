9267819eafba83dd894a6adf6475af8105db14cb
Tyler Schloesser <tylerschloesser@gmail.com>
2023-11-11 10:32:49 -0800

    configure pointer mode

--- packages/demo-gear/src/toolbar.tsx
+++ packages/demo-gear/src/toolbar.tsx
@@ -1,19 +1,23 @@
 import { useEffect, useState } from 'react'
 import { useInputState } from './state.js'
 import styles from './toolbar.module.scss'
-import { GEAR_SIZES } from './types.js'
+import { GEAR_SIZES, PointerMode } from './types.js'
 
 export function Toolbar() {
   const inputState = useInputState()
   const [gearSize, setGearSize] = useState<number>(inputState.current.gearSize)
+  const [pointerMode, setPointerMode] = useState<PointerMode>(
+    PointerMode.AddGear,
+  )
 
   useEffect(() => {
     inputState.current.gearSize = gearSize
-  }, [gearSize])
+    inputState.current.pointerMode = pointerMode
+  }, [gearSize, pointerMode])
 
   return (
     <div className={styles.container}>
-      <fieldset className={styles['gear-sizes']}>
+      <fieldset>
         <legend>Gear Size:</legend>
         {GEAR_SIZES.map((value) => (
           <label key={value}>
@@ -22,6 +26,19 @@ export function Toolbar() {
               type="radio"
               checked={gearSize === value}
               onChange={() => setGearSize(value)}
+            />
+          </label>
+        ))}
+      </fieldset>
+      <fieldset>
+        <legend>Pointer Mode:</legend>
+        {Object.values(PointerMode).map((value) => (
+          <label key={value}>
+            {value}
+            <input
+              type="radio"
+              checked={pointerMode === value}
+              onChange={() => setPointerMode(value)}
             />
           </label>
         ))}
