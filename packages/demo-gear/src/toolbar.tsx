5ed4917872cde5587511c1370460e7c5c86c9e49
Tyler Schloesser <tylerschloesser@gmail.com>
2023-11-11 10:17:41 -0800

    configure gear size

--- packages/demo-gear/src/toolbar.tsx
+++ packages/demo-gear/src/toolbar.tsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from 'react'
+import { useInputState } from './state.js'
 import styles from './toolbar.module.scss'
+import { GEAR_SIZES } from './types.js'
 
 export function Toolbar() {
-  return <div className={styles.container}>TODO</div>
+  const inputState = useInputState()
+  const [gearSize, setGearSize] = useState<number>(inputState.current.gearSize)
+
+  useEffect(() => {
+    inputState.current.gearSize = gearSize
+  }, [gearSize])
+
+  return (
+    <div className={styles.container}>
+      <fieldset>
+        {GEAR_SIZES.map((value) => (
+          <label key={value}>
+            {value}
+            <input
+              type="radio"
+              checked={gearSize === value}
+              onChange={() => setGearSize(value)}
+            />
+          </label>
+        ))}
+      </fieldset>
+    </div>
+  )
 }
