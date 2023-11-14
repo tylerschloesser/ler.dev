2f8e36d79c571f0b77ff02ee849201a9f2e89be2
Tyler Schloesser <tylerschloesser@gmail.com>
2023-11-14 00:05:46 -0800

    refactor render pointer

--- packages/demo-gear/src/render-pointer.ts
+++ packages/demo-gear/src/render-pointer.ts
@@ -2,7 +2,148 @@ import invariant from 'tiny-invariant'
 import { TILE_SIZE } from './const.js'
 import { renderConnection } from './render-connection.js'
 import { renderGear } from './render-gear.js'
-import { Pointer, PointerType, World } from './types.js'
+import {
+  AddGearPointer,
+  AddGearWithChainPointer,
+  ApplyForcePointer,
+  Pointer,
+  PointerType,
+  World,
+} from './types.js'
+
+type RenderPointerFn<T extends Pointer> = (args: {
+  pointer: T
+  context: CanvasRenderingContext2D
+  world: World
+}) => void
+
+const renderAddGearPointer: RenderPointerFn<
+  AddGearPointer
+> = ({ pointer, context, world }) => {
+  const { size, state } = pointer
+  if (!state) {
+    return
+  }
+  if (state.chain) {
+    context.beginPath()
+    context.lineWidth = 2
+    context.strokeStyle = 'white'
+    context.strokeRect(
+      state.position.x * TILE_SIZE,
+      state.position.y * TILE_SIZE,
+      TILE_SIZE,
+      TILE_SIZE,
+    )
+    context.closePath()
+  } else {
+    renderGear({
+      gear: {
+        position: state.position,
+        radius: size / 2,
+        angle: 0,
+      },
+      tint: state.valid
+        ? `hsla(120, 50%, 50%, .5)`
+        : `hsla(0, 50%, 50%, .5)`,
+      context,
+    })
+
+    for (const connection of state.connections) {
+      const gear2 = world.gears[connection.gearId]
+      invariant(gear2)
+      renderConnection({
+        context,
+        gear1: { position: state.position },
+        gear2,
+        type: connection.type,
+      })
+    }
+  }
+}
+
+const renderApplyForcePointer: RenderPointerFn<
+  ApplyForcePointer
+> = ({ pointer, context, world }) => {
+  const { state } = pointer
+  if (!state || !state.gearId) {
+    return
+  }
+  const gear = world.gears[state.gearId]
+  invariant(gear)
+
+  const { active } = state
+
+  context.beginPath()
+  context.lineWidth = 2
+  context.strokeStyle = active ? 'green' : 'white'
+  context.strokeRect(
+    (gear.position.x - (gear.radius - 0.5)) * TILE_SIZE,
+    (gear.position.y - (gear.radius - 0.5)) * TILE_SIZE,
+    TILE_SIZE * gear.radius * 2,
+    TILE_SIZE * gear.radius * 2,
+  )
+  context.closePath()
+}
+
+const renderAddGearWithChainPointer: RenderPointerFn<
+  AddGearWithChainPointer
+> = ({ pointer, context, world }) => {
+  const source = world.gears[pointer.sourceId]
+  invariant(source)
+
+  let chain = {
+    x: source.position.x,
+    y: source.position.y,
+    w: 1,
+    h: 1,
+  }
+
+  const { state } = pointer
+  if (state) {
+    renderGear({
+      gear: {
+        position: state.position,
+        radius: 0.5,
+        angle: 0,
+      },
+      tint: state.valid
+        ? `hsla(120, 50%, 50%, .5)`
+        : `hsla(0, 50%, 50%, .5)`,
+      context,
+    })
+
+    for (const connection of state.connections) {
+      const gear2 = world.gears[connection.gearId]
+      invariant(gear2)
+      renderConnection({
+        context,
+        gear1: { position: state.position },
+        gear2,
+        type: connection.type,
+      })
+    }
+
+    if (state.valid) {
+      chain = {
+        x: Math.min(chain.x, state.position.x),
+        y: Math.min(chain.y, state.position.y),
+        w: Math.abs(chain.x - state.position.x) + 1,
+        h: Math.abs(chain.y - state.position.y) + 1,
+      }
+    }
+  }
+
+  context.beginPath()
+  context.lineWidth = 2
+  context.strokeStyle = 'white'
+  context.strokeRect(
+    chain.x * TILE_SIZE,
+    chain.y * TILE_SIZE,
+    chain.w * TILE_SIZE,
+    chain.h * TILE_SIZE,
+  )
+  context.closePath()
+}
 
 export function renderPointer({
   pointer,
@@ -13,123 +154,19 @@ export function renderPointer({
   context: CanvasRenderingContext2D
   world: World
 }): void {
-  if (
-    pointer.type === PointerType.AddGear &&
-    pointer.state
-  ) {
-    const { size, state } = pointer
-    if (state.chain) {
-      context.beginPath()
-      context.lineWidth = 2
-      context.strokeStyle = 'white'
-      context.strokeRect(
-        state.position.x * TILE_SIZE,
-        state.position.y * TILE_SIZE,
-        TILE_SIZE,
-        TILE_SIZE,
-      )
-      context.closePath()
-    } else {
-      renderGear({
-        gear: {
-          position: state.position,
-          radius: size / 2,
-          angle: 0,
-        },
-        tint: state.valid
-          ? `hsla(120, 50%, 50%, .5)`
-          : `hsla(0, 50%, 50%, .5)`,
+  switch (pointer.type) {
+    case PointerType.AddGear:
+      renderAddGearPointer({ pointer, context, world })
+      break
+    case PointerType.AddGearWithChain:
+      renderAddGearWithChainPointer({
+        pointer,
         context,
+        world,
       })
-
-      for (const connection of state.connections) {
-        const gear2 = world.gears[connection.gearId]
-        invariant(gear2)
-        renderConnection({
-          context,
-          gear1: { position: state.position },
-          gear2,
-          type: connection.type,
-        })
-      }
-    }
-  }
-
-  if (
-    pointer.type === PointerType.ApplyForce &&
-    pointer.state?.gearId
-  ) {
-    const gear = world.gears[pointer.state.gearId]
-    const { active } = pointer.state
-    invariant(gear)
-
-    context.beginPath()
-    context.lineWidth = 2
-    context.strokeStyle = active ? 'green' : 'white'
-    context.strokeRect(
-      (gear.position.x - (gear.radius - 0.5)) * TILE_SIZE,
-      (gear.position.y - (gear.radius - 0.5)) * TILE_SIZE,
-      TILE_SIZE * gear.radius * 2,
-      TILE_SIZE * gear.radius * 2,
-    )
-    context.closePath()
-  }
-
-  if (pointer.type === PointerType.AddGearWithChain) {
-    const source = world.gears[pointer.sourceId]
-    invariant(source)
-
-    let chain = {
-      x: source.position.x,
-      y: source.position.y,
-      w: 1,
-      h: 1,
-    }
-
-    const { state } = pointer
-    if (state) {
-      renderGear({
-        gear: {
-          position: state.position,
-          radius: 0.5,
-          angle: 0,
-        },
-        tint: state.valid
-          ? `hsla(120, 50%, 50%, .5)`
-          : `hsla(0, 50%, 50%, .5)`,
-        context,
-      })
-
-      for (const connection of state.connections) {
-        const gear2 = world.gears[connection.gearId]
-        invariant(gear2)
-        renderConnection({
-          context,
-          gear1: { position: state.position },
-          gear2,
-          type: connection.type,
-        })
-      }
-
-      if (state.valid) {
-        chain = {
-          x: Math.min(chain.x, state.position.x),
-          y: Math.min(chain.y, state.position.y),
-          w: Math.abs(chain.x - state.position.x) + 1,
-          h: Math.abs(chain.y - state.position.y) + 1,
-        }
-      }
-    }
-
-    context.beginPath()
-    context.lineWidth = 2
-    context.strokeStyle = 'white'
-    context.strokeRect(
-      chain.x * TILE_SIZE,
-      chain.y * TILE_SIZE,
-      chain.w * TILE_SIZE,
-      chain.h * TILE_SIZE,
-    )
-    context.closePath()
+      break
+    case PointerType.ApplyForce:
+      renderApplyForcePointer({ pointer, context, world })
+      break
   }
 }
