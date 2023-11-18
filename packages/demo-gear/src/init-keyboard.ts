778e5b0a2eea612494b58ce2d83714130957be57
Tyler Schloesser <tylerschloesser@gmail.com>
2023-11-18 09:41:26 -0800

    fix keyboard

--- packages/demo-gear/src/init-keyboard.ts
+++ packages/demo-gear/src/init-keyboard.ts
@@ -1,10 +1,11 @@
-import { InitFn } from './types.js'
+import { InitFn, PointerMode } from './types.js'
 
 export const initKeyboard: InitFn = (state) => {
   window.addEventListener(
     'keyup',
     (e) => {
       if (e.key === 'q') {
+        state.pointer.mode = PointerMode.Free
         state.build = null
         state.accelerate = null
       }
