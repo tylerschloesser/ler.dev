8b17330e0ad24e84ea9963d795e5cefed4bef585
Tyler Schloesser <tylerschloesser@gmail.com>
2023-11-20 10:56:56 -0800

    refactor inner

--- packages/demo-gear/src/touch-toolbar.component.tsx
+++ packages/demo-gear/src/touch-toolbar.component.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { Dispatch, SetStateAction, useState } from 'react'
 import styles from './touch-toolbar.module.scss'
 import { AppState } from './types.js'
 
@@ -7,20 +7,32 @@ export interface TouchToolbarProps {
 }
 
 type View = 'main' | 'add-gear'
+type SetViewFn = Dispatch<SetStateAction<View>>
 
 export function TouchToolbar(props: TouchToolbarProps) {
   const [view, setView] = useState<View>('main')
-
+  const Inner = MainView
   return (
     <div className={styles.container}>
-      <button
-        className={styles.button}
-        onPointerUp={() => {
-          console.log('todo')
-        }}
-      >
-        Add Gear
-      </button>
+      <Inner state={props.state} setView={setView} />
     </div>
+  )
+}
+
+function MainView({
+  setView,
+}: {
+  state: AppState
+  setView: SetViewFn
+}) {
+  return (
+    <button
+      className={styles.button}
+      onPointerUp={() => {
+        setView('add-gear')
+      }}
+    >
+      Add Gear
+    </button>
   )
 }
