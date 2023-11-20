53f0e9c06bc37e9c2fa49e25cf722afc1428f903
Tyler Schloesser <tylerschloesser@gmail.com>
2023-11-20 11:57:43 -0800

    add missing files

---
+++ packages/demo-gear/src/hand.ts
@@ -0,0 +1,109 @@
+import invariant from 'tiny-invariant'
+import {
+  updateAccelerate,
+  updateAcceleratePosition,
+} from './accelerate.js'
+import {
+  executeBuild,
+  updateBuildPosition,
+} from './build.js'
+import {
+  AppState,
+  HandType,
+  PointerListenerFn,
+} from './types.js'
+
+export const moveHand: PointerListenerFn = (state, e) => {
+  const { pointer, hand } = state
+  updatePosition(state, e)
+  switch (e.type) {
+    case 'pointerenter': {
+      let tileX = Math.floor(pointer.position.x + 0.5)
+      let tileY = Math.floor(pointer.position.y + 0.5)
+      switch (hand?.type) {
+        case HandType.Build: {
+          updateBuildPosition(state, hand, tileX, tileY)
+          break
+        }
+        case HandType.Accelerate: {
+          updateAcceleratePosition(
+            state,
+            hand,
+            tileX,
+            tileY,
+          )
+          break
+        }
+      }
+      break
+    }
+    case 'pointerup': {
+      pointer.down = false
+      switch (hand?.type) {
+        case HandType.Build: {
+          executeBuild(state, hand)
+          break
+        }
+        case HandType.Accelerate: {
+          updateAccelerate(state, hand)
+          break
+        }
+      }
+      break
+    }
+    case 'pointerdown': {
+      pointer.down = true
+      switch (hand?.type) {
+        case HandType.Accelerate: {
+          updateAccelerate(state, hand)
+          break
+        }
+      }
+      break
+    }
+    case 'pointermove': {
+      let tileX = Math.floor(pointer.position.x + 0.5)
+      let tileY = Math.floor(pointer.position.y + 0.5)
+      switch (hand?.type) {
+        case HandType.Build: {
+          updateBuildPosition(state, hand, tileX, tileY)
+          break
+        }
+        case HandType.Accelerate: {
+          updateAcceleratePosition(
+            state,
+            hand,
+            tileX,
+            tileY,
+          )
+          break
+        }
+      }
+      break
+    }
+    case 'pointerleave': {
+      if (hand) {
+        hand.position = null
+      }
+      break
+    }
+    default: {
+      invariant(false)
+    }
+  }
+}
+
+function updatePosition(
+  state: AppState,
+  e: PointerEvent,
+): void {
+  const { canvas, pointer, tileSize, camera } = state
+  const vx = canvas.width
+  const vy = canvas.height
+  const x =
+    (e.offsetX - vx / 2) / tileSize + camera.position.x
+  const y =
+    (e.offsetY - vy / 2) / tileSize + camera.position.y
+  pointer.position.x = x
+  pointer.position.y = y
+}
---
+++ packages/demo-gear/src/pointer-mode.ts
@@ -0,0 +1,30 @@
+import invariant from 'tiny-invariant'
+import { moveCamera } from './camera.js'
+import { moveHand } from './hand.js'
+import { AppState, PointerMode } from './types.js'
+
+export function updatePointerMode(
+  state: AppState,
+  mode: PointerMode,
+): void {
+  if (state.pointer.mode === mode) {
+    return
+  }
+  state.pointer.mode = mode
+
+  switch (mode) {
+    case PointerMode.Free: {
+      state.pointerListeners.add(moveCamera)
+      state.pointerListeners.delete(moveHand)
+      break
+    }
+    case PointerMode.Hand: {
+      state.pointerListeners.add(moveHand)
+      state.pointerListeners.delete(moveCamera)
+      break
+    }
+    default: {
+      invariant(false)
+    }
+  }
+}
