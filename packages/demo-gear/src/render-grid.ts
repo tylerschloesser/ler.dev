2a468d42a59f2acf45f3f3836d7269bd08eca6ba
Tyler Schloesser <tylerschloesser@gmail.com>
2023-11-20 10:41:09 -0800

    update grid

--- packages/demo-gear/src/render-grid.ts
+++ packages/demo-gear/src/render-grid.ts
@@ -23,31 +23,35 @@ export function renderGrid(
     Math.ceil((camera.position.y + vy / 2) / 2) * 2,
   )
 
-  context.beginPath()
-  context.lineWidth = 1
-  context.strokeStyle = Color.GridOdd
-  for (let y = tly + 1; y < bry; y += 2) {
-    context.moveTo(tlx * tileSize, y * tileSize)
-    context.lineTo(brx * tileSize, y * tileSize)
+  if (state.camera.zoom > 0.5) {
+    context.beginPath()
+    context.lineWidth = 1
+    context.strokeStyle = Color.GridOdd
+    for (let y = tly + 1; y < bry; y += 2) {
+      context.moveTo(tlx * tileSize, y * tileSize)
+      context.lineTo(brx * tileSize, y * tileSize)
+    }
+    for (let x = tlx + 1; x < brx; x += 2) {
+      context.moveTo(x * tileSize, tly * tileSize)
+      context.lineTo(x * tileSize, bry * tileSize)
+    }
+    context.stroke()
+    context.closePath()
   }
-  for (let x = tlx + 1; x < brx; x += 2) {
-    context.moveTo(x * tileSize, tly * tileSize)
-    context.lineTo(x * tileSize, bry * tileSize)
-  }
-  context.stroke()
-  context.closePath()
 
-  context.beginPath()
-  context.lineWidth = 1
-  context.strokeStyle = Color.GridEven
-  for (let y = tly; y <= bry; y += 2) {
-    context.moveTo(tlx * tileSize, y * tileSize)
-    context.lineTo(brx * tileSize, y * tileSize)
+  if (state.camera.zoom > 0.25) {
+    context.beginPath()
+    context.lineWidth = 1
+    context.strokeStyle = Color.GridEven
+    for (let y = tly; y <= bry; y += 2) {
+      context.moveTo(tlx * tileSize, y * tileSize)
+      context.lineTo(brx * tileSize, y * tileSize)
+    }
+    for (let x = tlx; x <= brx; x += 2) {
+      context.moveTo(x * tileSize, tly * tileSize)
+      context.lineTo(x * tileSize, bry * tileSize)
+    }
+    context.stroke()
+    context.closePath()
   }
-  for (let x = tlx; x <= brx; x += 2) {
-    context.moveTo(x * tileSize, tly * tileSize)
-    context.lineTo(x * tileSize, bry * tileSize)
-  }
-  context.stroke()
-  context.closePath()
 }
