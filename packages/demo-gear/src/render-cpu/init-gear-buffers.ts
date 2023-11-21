a0e96d35b18ab96b13fdff75789716135c869778
Tyler Schloesser <tylerschloesser@gmail.com>
2023-11-21 09:31:18 -0800

    refactor init circle

--- packages/demo-gear/src/render-cpu/init-gear-buffers.ts
+++ packages/demo-gear/src/render-cpu/init-gear-buffers.ts
@@ -8,30 +8,36 @@ export function initGearBuffers(
   const gears: GpuState['buffers']['gears'] = {}
 
   for (const radius of GEAR_RADIUSES) {
-    const vertex = gl.createBuffer()
-    invariant(vertex)
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertex)
-
-    const teeth = radius * TEETH
-
-    const data = new Float32Array((teeth + 1) * 2 + 2)
-
-    data[0] = 0.0
-    data[1] = 0.0
-
-    for (let i = 0; i <= teeth; i++) {
-      const angle = TWO_PI * (i / teeth)
-      data[2 + i * 2 + 0] = Math.cos(angle)
-      data[2 + i * 2 + 1] = Math.sin(angle)
-    }
-
-    // TODO
-    gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW)
-
     gears[radius] = {
-      circle: { vertex, count: data.length / 2 },
+      circle: initCircle(gl, radius),
     }
   }
 
   return gears
+}
+
+function initCircle(
+  gl: WebGL2RenderingContext,
+  radius: number,
+): GpuState['buffers']['gears'][0]['circle'] {
+  const vertex = gl.createBuffer()
+  invariant(vertex)
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertex)
+
+  const teeth = radius * TEETH
+
+  const data = new Float32Array((teeth + 1) * 2 + 2)
+
+  data[0] = 0.0
+  data[1] = 0.0
+
+  for (let i = 0; i <= teeth; i++) {
+    const angle = TWO_PI * (i / teeth)
+    data[2 + i * 2 + 0] = Math.cos(angle)
+    data[2 + i * 2 + 1] = Math.sin(angle)
+  }
+
+  gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW)
+
+  return { vertex, count: data.length / 2 }
 }
