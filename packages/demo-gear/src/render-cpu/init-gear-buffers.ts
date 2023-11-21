8d699972af830fe6b669cb394f9ec86faba15897
Tyler Schloesser <tylerschloesser@gmail.com>
2023-11-21 08:12:53 -0800

    add placeholder buffer

--- packages/demo-gear/src/render-cpu/init-gear-buffers.ts
+++ packages/demo-gear/src/render-cpu/init-gear-buffers.ts
@@ -1,7 +1,19 @@
+import invariant from 'tiny-invariant'
+import { GEAR_RADIUSES } from '../const.js'
 import { GpuState } from '../render-gpu/types.js'
 
 export function initGearBuffers(
   gl: WebGL2RenderingContext,
 ): GpuState['buffers']['gears'] {
-  return {}
+  const gears: GpuState['buffers']['gears'] = {}
+
+  for (const radius of GEAR_RADIUSES) {
+    const vertex = gl.createBuffer()
+    invariant(vertex)
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertex)
+
+    gears[radius] = { vertex }
+  }
+
+  return gears
 }
