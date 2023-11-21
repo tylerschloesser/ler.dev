7f88856d7ef056cfc6f7da566d76e28696771254
Tyler Schloesser <tylerschloesser@gmail.com>
2023-11-20 20:20:48 -0800

    square working

--- packages/demo-gear/src/render-gpu/render.ts
+++ packages/demo-gear/src/render-gpu/render.ts
@@ -10,4 +10,19 @@ export function render(
 
   gl.clearColor(1, 0, 0, 1)
   gl.clear(gl.COLOR_BUFFER_BIT)
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, gpu.buffers.square)
+  gl.vertexAttribPointer(
+    gpu.programs.main.attributes.vertex,
+    2,
+    gl.FLOAT,
+    false,
+    0,
+    0,
+  )
+  gl.enableVertexAttribArray(
+    gpu.programs.main.attributes.vertex,
+  )
+
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4)
 }
