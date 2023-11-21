969b20984d67ae242719042cc7bf5553ad304f2b
Tyler Schloesser <tylerschloesser@gmail.com>
2023-11-21 08:14:27 -0800

    buffer test data

--- packages/demo-gear/src/render-cpu/init-gear-buffers.ts
+++ packages/demo-gear/src/render-cpu/init-gear-buffers.ts
@@ -12,6 +12,19 @@ export function initGearBuffers(
     invariant(vertex)
     gl.bindBuffer(gl.ARRAY_BUFFER, vertex)
 
+    // TODO
+    gl.bufferData(
+      gl.ARRAY_BUFFER,
+      // prettier-ignore
+      new Float32Array([
+        -1.0, -1.0, 
+        -1.0, 1.0, 
+        1.0, -1.0, 
+        1.0, 1.0,
+      ]),
+      gl.STATIC_DRAW,
+    )
+
     gears[radius] = { vertex }
   }
 
