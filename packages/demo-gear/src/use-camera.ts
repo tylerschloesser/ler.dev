8c10d28f109abd8b8a2c8af2b24f154c4b2dd5c0
Tyler Schloesser <tylerschloesser@gmail.com>
2023-11-26 13:31:55 -0800

    save camera

--- packages/demo-gear/src/use-camera.ts
+++ packages/demo-gear/src/use-camera.ts
@@ -1,17 +1,29 @@
 import { Camera } from './types.js'
 import { throttle } from './util.js'
 
+const DEFAULT_CAMERA: Camera = {
+  position: { x: 0, y: 0 },
+  zoom: 0.5,
+}
+
+function loadCamera(): Camera {
+  const json = localStorage.getItem('camera')
+  if (json) {
+    return JSON.parse(json)
+  }
+  return DEFAULT_CAMERA
+}
+
+function saveCamera(camera: Camera): void {
+  localStorage.setItem('camera', JSON.stringify(camera))
+}
+
 export function useCamera(): {
   camera: Camera
   saveCamera(camera: Camera): void
 } {
   return {
-    camera: {
-      position: { x: 0, y: 0 },
-      zoom: 0.5,
-    },
-    saveCamera: throttle((camera: Camera) => {
-      console.log('todo save camera')
-    }, 100),
+    camera: loadCamera(),
+    saveCamera: throttle(saveCamera, 100),
   }
 }
