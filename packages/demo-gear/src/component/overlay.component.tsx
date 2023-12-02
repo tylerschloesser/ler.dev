2b7e82bc88ec66a4b89eb76a1b86a3748a6c509b
Tyler Schloesser <tylerschloesser@gmail.com>
2023-12-02 08:47:26 -0800

    update initial scroll

--- packages/demo-gear/src/component/overlay.component.tsx
+++ packages/demo-gear/src/component/overlay.component.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './overlay.module.scss'
 
 export type OverlayProps = React.PropsWithChildren<{
@@ -9,6 +9,17 @@ export function Overlay({
   position = 'bottom',
   children,
 }: OverlayProps) {
+  const [outer, setOuter] = useState<HTMLDivElement | null>(
+    null,
+  )
+  useEffect(() => {
+    if (!outer) return
+    // start out scrolled all the way to the right
+    outer.scrollTo({
+      left: outer.scrollWidth - outer.clientWidth,
+    })
+  }, [outer])
+
   return (
     <div
       className={
@@ -16,6 +27,7 @@ export function Overlay({
           ? styles['outer-top']
           : styles['outer-bottom']
       }
+      ref={setOuter}
     >
       <div className={styles.inner}>{children}</div>
     </div>
