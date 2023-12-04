c44b8ddee674778e3314729f1e982ea3cf94ea41
Tyler Schloesser <tylerschloesser@gmail.com>
2023-12-03 16:27:37 -0800

    add center id listener

--- packages/demo-gear/src/component/add-resource.component.tsx
+++ packages/demo-gear/src/component/add-resource.component.tsx
@@ -1,9 +1,24 @@
+import { use, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { CenterTileIdListener } from '../types.js'
 import styles from './add-resource.module.scss'
+import { AppContext } from './context.js'
 import { Overlay } from './overlay.component.js'
 
 export function AddResource() {
+  const state = use(AppContext)
   const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!state) {
+      return
+    }
+    const centerTileIdListener: CenterTileIdListener =
+      () => {
+        const tile = state.world.tiles[state.centerTileId]
+      }
+  }, [state])
+
   return (
     <Overlay>
       <button
