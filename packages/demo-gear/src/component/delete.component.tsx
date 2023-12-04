9ad1f01440011e0ac8e685caa79eb0c0efebe46a
Tyler Schloesser <tylerschloesser@gmail.com>
2023-12-04 14:28:46 -0800

    add back button

--- packages/demo-gear/src/component/delete.component.tsx
+++ packages/demo-gear/src/component/delete.component.tsx
@@ -1,5 +1,20 @@
+import { useNavigate } from 'react-router-dom'
+import styles from './delete.module.scss'
 import { Overlay } from './overlay.component.js'
 
 export function Delete() {
-  return <Overlay>TODO</Overlay>
+  const navigate = useNavigate()
+
+  return (
+    <Overlay>
+      <button
+        className={styles.button}
+        onPointerUp={() => {
+          navigate('..')
+        }}
+      >
+        Back
+      </button>
+    </Overlay>
+  )
 }
