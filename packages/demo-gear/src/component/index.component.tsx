377ba66145c662eb7baf9e047107f8f8444acad0
Tyler Schloesser <tylerschloesser@gmail.com>
2023-12-03 16:14:42 -0800

    add tools button

--- packages/demo-gear/src/component/index.component.tsx
+++ packages/demo-gear/src/component/index.component.tsx
@@ -1,5 +1,19 @@
+import { useNavigate } from 'react-router-dom'
+import styles from './index.module.scss'
 import { Overlay } from './overlay.component.js'
 
 export function Index() {
-  return <Overlay>TODO</Overlay>
+  const navigate = useNavigate()
+  return (
+    <Overlay>
+      <button
+        className={styles.button}
+        onClick={() => {
+          navigate('tools')
+        }}
+      >
+        Tools
+      </button>
+    </Overlay>
+  )
 }
