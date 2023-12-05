827def5e820663206980dd4435fe584b73437e67
Tyler Schloesser <tylerschloesser@gmail.com>
2023-12-04 18:23:10 -0800

    refactor render belt

--- packages/demo-gear/src/render/render-belt.ts
+++ packages/demo-gear/src/render/render-belt.ts
@@ -1,10 +1,7 @@
-import { mat4, vec3 } from 'gl-matrix'
 import { Belt, IAppContext } from '../types.js'
 import { Color } from './color.js'
+import { batchRenderRect } from './render-rect.js'
 import { GpuState } from './types.js'
-
-const model = mat4.create()
-const v3 = vec3.create()
 
 export function renderBelt(
   _context: IAppContext,
@@ -13,63 +10,9 @@ export function renderBelt(
   path: Belt['path'],
   color: Color,
 ) {
-  const { fillRect } = gpu.programs
-  gl.useProgram(fillRect.program)
-
-  const { view, projection } = gpu.matrices
-  gl.uniformMatrix4fv(fillRect.uniforms.view, false, view)
-  gl.uniformMatrix4fv(
-    fillRect.uniforms.projection,
-    false,
-    projection,
-  )
-
-  gl.uniform4f(
-    fillRect.uniforms.color,
-    color.r,
-    color.g,
-    color.b,
-    color.a,
-  )
+  const render = batchRenderRect(gl, gpu, color)
 
   for (const { x, y } of path) {
-    mat4.identity(model)
-
-    // the model is -1 to 1 because it's easier to scale
-    // to the radius for gears...
-    // so we need to offset and scale by .5
-    //
-    // TODO clean this up for real...
-    //
-
-    v3[0] = x + 0.5
-    v3[1] = y + 0.5
-    v3[2] = 0
-    mat4.translate(model, model, v3)
-
-    v3[0] = 0.5
-    v3[1] = 0.5
-    v3[2] = 0
-    mat4.scale(model, model, v3)
-
-    gl.uniformMatrix4fv(
-      fillRect.uniforms.model,
-      false,
-      model,
-    )
-
-    const buffer = gpu.buffers.square
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
-    gl.vertexAttribPointer(
-      fillRect.attributes.vertex,
-      2,
-      gl.FLOAT,
-      false,
-      0,
-      0,
-    )
-    gl.enableVertexAttribArray(fillRect.attributes.vertex)
-
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4)
+    render(x, y, 1, 1)
   }
 }
--- packages/demo-gear/src/render/render-rect.ts
+++ packages/demo-gear/src/render/render-rect.ts
@@ -5,6 +5,65 @@ import { GpuState } from './types.js'
 const model = mat4.create()
 const v = vec3.create()
 
+export function batchRenderRect(
+  gl: WebGL2RenderingContext,
+  gpu: GpuState,
+  color: Color,
+) {
+  const { fillRect } = gpu.programs
+  gl.useProgram(fillRect.program)
+
+  const { view, projection } = gpu.matrices
+  gl.uniformMatrix4fv(fillRect.uniforms.view, false, view)
+  gl.uniformMatrix4fv(
+    fillRect.uniforms.projection,
+    false,
+    projection,
+  )
+
+  gl.uniform4f(
+    fillRect.uniforms.color,
+    color.r,
+    color.g,
+    color.b,
+    color.a,
+  )
+
+  return (x: number, y: number, w: number, h: number) => {
+    mat4.identity(model)
+
+    v[0] = x
+    v[1] = y
+    v[2] = 0
+    mat4.translate(model, model, v)
+
+    v[0] = w
+    v[1] = h
+    v[2] = 0
+    mat4.scale(model, model, v)
+
+    gl.uniformMatrix4fv(
+      fillRect.uniforms.model,
+      false,
+      model,
+    )
+
+    const buffer = gpu.buffers.fillRect
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+    gl.vertexAttribPointer(
+      fillRect.attributes.vertex,
+      2,
+      gl.FLOAT,
+      false,
+      0,
+      0,
+    )
+    gl.enableVertexAttribArray(fillRect.attributes.vertex)
+
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4)
+  }
+}
+
 export function renderRect(
   gl: WebGL2RenderingContext,
   gpu: GpuState,
