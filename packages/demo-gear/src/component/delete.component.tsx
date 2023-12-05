eaa01a57a061077867614d7e25d32388712375e7
Tyler Schloesser <tylerschloesser@gmail.com>
2023-12-04 21:06:15 -0800

    refactor delete

--- packages/demo-gear/src/component/delete.component.tsx
+++ packages/demo-gear/src/component/delete.component.tsx
@@ -10,21 +10,15 @@ import {
   useSearchParams,
 } from 'react-router-dom'
 import invariant from 'tiny-invariant'
-import { addBelt } from '../belt.js'
+import { executeDelete } from '../delete.js'
 import {
-  BeltId,
-  BeltPath,
   CameraListenerFn,
-  ConnectionType,
   DeleteHand,
   HandType,
   IAppContext,
   SimpleVec2,
 } from '../types.js'
-import {
-  iterateGearTileIds,
-  iterateTiles,
-} from '../util.js'
+import { iterateTiles } from '../util.js'
 import { AppContext } from './context.js'
 import styles from './delete.module.scss'
 import { Overlay } from './overlay.component.js'
@@ -177,102 +171,7 @@ export function Delete() {
             className={styles.button}
             onPointerUp={() => {
               if (disabled) return
-
-              for (const gearId of hand.current.gearIds) {
-                const gear = context.world.gears[gearId]
-                invariant(gear)
-
-                for (const tileId of iterateGearTileIds(
-                  gear.position,
-                  gear.radius,
-                )) {
-                  const tile = context.world.tiles[tileId]
-                  invariant(tile)
-
-                  if (tile.attachedGearId === gearId) {
-                    delete tile.attachedGearId
-                  } else {
-                    invariant(tile.gearId === gearId)
-                    if (tile.attachedGearId) {
-                      tile.gearId = tile.attachedGearId
-                      delete tile.attachedGearId
-                    } else {
-                      delete tile.gearId
-                    }
-                  }
-
-                  if (!tile.gearId && !tile.resourceType) {
-                    delete context.world.tiles[tileId]
-                  }
-                }
-
-                for (const connection of gear.connections) {
-                  invariant(
-                    connection.type !==
-                      ConnectionType.enum.Belt,
-                    'TODO support belt connections',
-                  )
-                  const neighbor =
-                    context.world.gears[connection.gearId]
-                  invariant(neighbor)
-                  const index =
-                    neighbor.connections.findIndex(
-                      (c) =>
-                        c.type !==
-                          ConnectionType.enum.Belt &&
-                        c.gearId === gear.id,
-                    )
-                  invariant(index !== -1)
-                  neighbor.connections.splice(index, 1)
-                }
-
-                delete context.world.gears[gearId]
-              }
-
-              const beltIds = new Set<BeltId>()
-
-              for (const tileId of hand.current.tileIds) {
-                const tile = context.world.tiles[tileId]
-                invariant(tile)
-                if (tile.beltId) {
-                  beltIds.add(tile.beltId)
-                }
-                if (tile.resourceType) {
-                  delete tile.resourceType
-                }
-                if (Object.keys(tile).length === 0) {
-                  delete context.world.tiles[tileId]
-                }
-              }
-
-              for (const beltId of beltIds) {
-                const belt = context.world.belts[beltId]
-                invariant(belt)
-                const { path: oldPath } = belt
-                delete context.world.belts[beltId]
-
-                let newPath: BeltPath = []
-                for (const cell of oldPath) {
-                  const { position } = cell
-                  const tileId = `${position.x}.${position.y}`
-                  const tile = context.world.tiles[tileId]
-                  invariant(tile)
-                  invariant(tile.beltId === beltId)
-                  delete tile.beltId
-                  if (hand.current.tileIds.has(tileId)) {
-                    if (newPath.length) {
-                      addBelt(context.world, newPath)
-                      newPath = []
-                    }
-                  } else {
-                    newPath.push(cell)
-                  }
-                }
-                if (newPath.length) {
-                  addBelt(context.world, newPath)
-                }
-              }
-
+              executeDelete(context, hand.current)
               checkDelete(
                 context,
                 hand.current,
