b38c7a5d5d100583147d4aa22bbb154c444a20e3
Tyler Schloesser <tylerschloesser@gmail.com>
2023-12-13 16:15:20 -0800

    refactor add resource

--- packages/demo-gear/src/component/add-resource.component.tsx
+++ packages/demo-gear/src/component/add-resource.component.tsx
@@ -2,63 +2,30 @@ import {
   use,
   useCallback,
   useEffect,
+  useMemo,
   useState,
 } from 'react'
 import { useNavigate } from 'react-router-dom'
 import invariant from 'tiny-invariant'
 import {
+  AddResourceHand,
   CameraListenerFn,
   HandType,
   ResourceType,
+  SimpleVec2,
 } from '../types.js'
+import { incrementBuildVersion } from '../util.js'
 import styles from './add-resource.module.scss'
 import { AppContext } from './context.js'
 import { Overlay } from './overlay.component.js'
+import { useWorldBuildVersion } from './use-world-build-version.js'
 
 export function AddResource() {
-  const context = use(AppContext)
   const navigate = useNavigate()
-  const [valid, setValid] = useState<boolean>(false)
 
-  useEffect(() => {
-    context.hand = {
-      type: HandType.AddResource,
-      position: { x: 0, y: 0 },
-      valid,
-    }
-
-    const cameraListener: CameraListenerFn = () => {
-      const x = Math.floor(context.camera.position.x)
-      const y = Math.floor(context.camera.position.y)
-      const tileId = `${x}.${y}`
-      invariant(context.hand?.type === HandType.AddResource)
-      context.hand.position.x = x
-      context.hand.position.y = y
-
-      const tile = context.world.tiles[tileId]
-      setValid((context.hand.valid = !tile?.resourceType))
-    }
-    context.cameraListeners.add(cameraListener)
-    cameraListener(context)
-    return () => {
-      context.hand = null
-      context.cameraListeners.delete(cameraListener)
-    }
-  }, [context])
-
-  const addResource = useCallback(() => {
-    invariant(context.hand?.type === HandType.AddResource)
-    if (!context.hand.valid) return
-    const { x, y } = context.hand.position
-    const tileId = `${x}.${y}`
-    let tile = context.world.tiles[tileId]
-    invariant(!tile?.resourceType)
-    if (!tile) {
-      tile = context.world.tiles[tileId] = {}
-    }
-    tile.resourceType = ResourceType.enum.Fuel
-    setValid((context.hand.valid = false))
-  }, [context])
+  const position = useHandPosition()
+  const hand = useHand(position)
+  const addResourceButton = useAddResourceButton(hand)
 
   return (
     <Overlay>
@@ -71,12 +38,94 @@ export function AddResource() {
         Back
       </button>
       <button
-        disabled={!valid}
+        disabled={addResourceButton.disabled}
         className={styles.button}
-        onPointerUp={addResource}
+        onPointerUp={addResourceButton.onPointerUp}
       >
         Add Resource
       </button>
     </Overlay>
   )
+}
+
+function useHandPosition(): SimpleVec2 {
+  const context = use(AppContext)
+
+  const [position, setPosition] = useState<SimpleVec2>({
+    x: 0,
+    y: 0,
+  })
+
+  useEffect(() => {
+    const listener: CameraListenerFn = () => {
+      const x = Math.floor(context.camera.position.x)
+      const y = Math.floor(context.camera.position.y)
+
+      setPosition((prev) => {
+        if (prev.x === x && prev.y === y) {
+          return prev
+        }
+        return { x, y }
+      })
+    }
+
+    context.cameraListeners.add(listener)
+    listener(context)
+
+    return () => {
+      context.cameraListeners.delete(listener)
+    }
+  }, [])
+
+  return position
+}
+
+function useHand(position: SimpleVec2): AddResourceHand {
+  const context = use(AppContext)
+  const buildVersion = useWorldBuildVersion()
+
+  const valid = useMemo(() => {
+    const tileId = `${position.x}.${position.y}`
+    const tile = context.world.tiles[tileId]
+    return !tile?.resourceType
+  }, [position, context, buildVersion])
+
+  const hand = useMemo<AddResourceHand>(
+    () => ({
+      type: HandType.AddResource,
+      position,
+      valid,
+    }),
+    [position, valid],
+  )
+
+  useEffect(() => {
+    context.hand = hand
+    return () => {
+      context.hand = null
+    }
+  }, [hand])
+
+  return hand
+}
+
+function useAddResourceButton(hand: AddResourceHand) {
+  const context = use(AppContext)
+  const disabled = !hand.valid
+
+  const onPointerUp = useCallback(() => {
+    if (disabled) return
+
+    const tileId = `${hand.position.x}.${hand.position.y}`
+    let tile = context.world.tiles[tileId]
+    if (!tile) {
+      tile = context.world.tiles[tileId] = {}
+    }
+    invariant(!tile.resourceType)
+    tile.resourceType = ResourceType.enum.Fuel
+
+    incrementBuildVersion(context)
+  }, [hand, context])
+
+  return { disabled, onPointerUp }
 }
