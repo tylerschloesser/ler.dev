a674421662d6c90d63e5dc5e6cd320d33d122b66
Tyler Schloesser <tylerschloesser@gmail.com>
2023-12-13 20:42:40 -0800

    update max tile size

--- packages/demo-gear/src/tick-belt.ts
+++ packages/demo-gear/src/tick-belt.ts
@@ -1,12 +1,16 @@
 import invariant from 'tiny-invariant'
-import { Belt, World } from './types.js'
+import { Belt, EntityType, World } from './types.js'
 
 export function tickBelt(
   world: World,
   belt: Belt,
   elapsed: number,
 ): void {
-  for (const item of belt.items) {
+  const remove = new Array<number>()
+  for (let i = 0; i < belt.items.length; i++) {
+    const item = belt.items.at(0)
+    invariant(item)
+
     invariant(item.position >= 0)
     invariant(item.position <= 1)
 
@@ -16,12 +20,18 @@ export function tickBelt(
     if (nextPosition > 1) {
       const next = getNextBelt(world, belt)
       if (next) {
+        item.position = nextPosition - 1
+        next.items.unshift(item)
+        remove.push(i)
       } else {
         item.position = 1
       }
     } else if (nextPosition < 0) {
       const prev = getPrevBelt(world, belt)
       if (prev) {
+        item.position = nextPosition + 1
+        prev.items.push(item)
+        remove.push(i)
       } else {
         item.position = 0
       }
@@ -29,12 +39,39 @@ export function tickBelt(
       item.position = nextPosition
     }
   }
+
+  for (const i of remove) {
+    invariant(i < belt.items.length)
+    belt.items.splice(i, 1)
+  }
 }
 
 function getNextBelt(
   world: World,
   belt: Belt,
 ): Belt | null {
+  if (belt.type === EntityType.enum.BeltIntersection) {
+    // TODO
+    return null
+  }
+
+  if (belt.direction === 'x') {
+    const tileId = `${belt.position.x + 1}.${
+      belt.position.y
+    }`
+    const tile = world.tiles[tileId]
+    if (!tile?.entityId) return null
+    const entity = world.entities[tile.entityId]
+    invariant(entity)
+    if (entity.type !== EntityType.enum.Belt) {
+      return null
+    }
+    if (entity.direction !== 'x') {
+      return null
+    }
+    return entity
+  }
+
   return null
 }
 
