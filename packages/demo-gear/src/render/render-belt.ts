3fcd5691b1ede2359f34927dc32aa2713a155181
Tyler Schloesser <tylerschloesser@gmail.com>
2023-12-15 15:49:28 -0800

    refactor render belt item

--- packages/demo-gear/src/render/render-belt.ts
+++ packages/demo-gear/src/render/render-belt.ts
@@ -1,6 +1,8 @@
 import invariant from 'tiny-invariant'
 import {
   Belt,
+  BeltItem,
+  BeltTurn,
   IAppContext,
   ItemType,
   Rotation,
@@ -47,87 +49,84 @@ function renderLine(
 
 function renderBeltItem(
   render: RenderFn,
-  rotation: Rotation,
-  x: number,
-  y: number,
-  itemType: ItemType,
-  position: number,
+  belt: Belt,
+  item: BeltItem,
 ): void {
   const border = 0.1
   const padding = 0.2
 
-  invariant(itemType === ItemType.enum.Fuel)
+  invariant(item.type === ItemType.enum.Fuel)
 
   render(
-    padding + (position - 0.5),
+    padding + (item.position - 0.5),
     padding + 0,
     1 - padding * 2,
     1 - padding * 2,
     FUEL_COLOR,
     0.1,
-    rotation,
-    x,
-    y,
+    belt.rotation,
+    belt.position.x,
+    belt.position.y,
     1,
     1,
   )
 
   // top border
   render(
-    padding + (position - 0.5),
+    padding + (item.position - 0.5),
     padding + 0,
     1 - padding * 2,
     border,
     ITEM_BORDER,
     0.1,
-    rotation,
-    x,
-    y,
+    belt.rotation,
+    belt.position.x,
+    belt.position.y,
     1,
     1,
   )
 
   // bottom border
   render(
-    padding + (position - 0.5),
+    padding + (item.position - 0.5),
     1 - padding + 0 - border,
     1 - padding * 2,
     border,
     ITEM_BORDER,
     0.1,
-    rotation,
-    x,
-    y,
+    belt.rotation,
+    belt.position.x,
+    belt.position.y,
     1,
     1,
   )
 
   // left border
   render(
-    padding + (position - 0.5),
+    padding + (item.position - 0.5),
     padding + 0,
     border,
     1 - padding * 2,
     ITEM_BORDER,
     0.1,
-    rotation,
-    x,
-    y,
+    belt.rotation,
+    belt.position.x,
+    belt.position.y,
     1,
     1,
   )
 
   // right border
   render(
-    1 - padding + (position - 0.5) - border,
+    1 - padding + (item.position - 0.5) - border,
     padding + 0,
     border,
     1 - padding * 2,
     ITEM_BORDER,
     0.1,
-    rotation,
-    x,
-    y,
+    belt.rotation,
+    belt.position.x,
+    belt.position.y,
     1,
     1,
   )
@@ -161,14 +160,7 @@ export function renderBelt(
   )
 
   for (const item of belt.items) {
-    renderBeltItem(
-      render,
-      belt.rotation,
-      x,
-      y,
-      item.type,
-      item.position,
-    )
+    renderBeltItem(render, belt, item)
   }
 
   if (tint) {
