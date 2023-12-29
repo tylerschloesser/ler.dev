505a96094ad7d33fd0cc8284327ab2f6783ef89e
Tyler Schloesser <tylerschloesser@gmail.com>
2023-12-28 18:37:20 -0800

    add belt path

--- packages/demo-gear/src/derived.ts
+++ packages/demo-gear/src/derived.ts
@@ -1,6 +1,15 @@
 import invariant from 'tiny-invariant'
-import { BeltPath, Derived } from './types-derived.js'
-import { Entity } from './types-entity.js'
+import {
+  BeltPath,
+  BeltPathEntity,
+  Derived,
+} from './types-derived.js'
+import {
+  BeltEntity,
+  Entity,
+  EntityId,
+  entityType,
+} from './types-entity.js'
 import { Origin } from './types-origin.js'
 
 type Tiles = Derived['tiles']
@@ -29,7 +38,107 @@ export function initBeltPaths(
   tiles: Tiles,
 ): BeltPath[] {
   const beltPaths: BeltPath[] = []
+
+  const seen = new Set<EntityId>()
+  for (const root of Object.values(origin.entities)) {
+    if (
+      root.type !== entityType.enum.Belt ||
+      seen.has(root.id)
+    ) {
+      continue
+    }
+
+    const beltPath = getBeltPath(origin, tiles, root)
+    for (const entityId of Object.keys(beltPath.entities)) {
+      invariant(!seen.has(entityId))
+      seen.add(entityId)
+    }
+
+    beltPaths.push(beltPath)
+  }
+
   return beltPaths
+}
+
+function getAdjacentBelts(
+  origin: Origin,
+  tiles: Tiles,
+  root: BeltEntity,
+): BeltEntity[] {
+  return []
+}
+
+function* iterateBeltPath(
+  origin: Origin,
+  tiles: Tiles,
+  seen: Set<BeltEntity>,
+  root: BeltEntity,
+  next: BeltEntity | undefined,
+) {
+  let prev = root
+  while (next) {
+    yield next
+
+    if (seen.has(next)) {
+      // the only reason this belt should be seen is if there is a loop
+      invariant(next === root)
+      return
+    }
+    seen.add(next)
+
+    const adjacent = getAdjacentBelts(origin, tiles, next)
+    invariant(adjacent.length <= 2)
+    const [a, b] = adjacent
+    if (a === prev) {
+      prev = next
+      next = b
+    } else if (b === prev) {
+      prev = next
+      next = a
+    } else {
+      invariant(false)
+    }
+  }
+}
+
+function getBeltPath(
+  origin: Origin,
+  tiles: Tiles,
+  root: BeltEntity,
+): BeltPath {
+  let loop = false
+  const belts = new Array<BeltEntity>(root)
+  const seen = new Set<BeltEntity>([root])
+
+  const adjacent = getAdjacentBelts(origin, tiles, root)
+  invariant(adjacent.length <= 2)
+
+  for (const left of iterateBeltPath(
+    origin,
+    tiles,
+    seen,
+    root,
+    adjacent[0],
+  )) {
+    if (left === root) {
+      loop = true
+      break
+    }
+    belts.unshift(left)
+  }
+  for (const right of iterateBeltPath(
+    origin,
+    tiles,
+    seen,
+    root,
+    adjacent[1],
+  )) {
+    invariant(right !== root)
+    belts.push(right)
+  }
+
+  const entities = new Array<BeltPathEntity>()
+  return { entities, loop }
 }
 
 function* iterateTilesIds(entity: Entity) {
