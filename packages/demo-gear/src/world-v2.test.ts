11450acce0dadc357f793f6ccb7df91cd7846c3e
Tyler Schloesser <tylerschloesser@gmail.com>
2023-12-28 16:46:56 -0800

    add test

---
+++ packages/demo-gear/src/world-v2.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, test } from '@jest/globals'
+
+describe('world-v2', () => {
+  describe('initWorld', () => {
+    test('todo', () => {
+      expect(false).toBe(false)
+    })
+  })
+})
