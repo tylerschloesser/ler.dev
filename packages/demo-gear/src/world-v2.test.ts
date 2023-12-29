4b714d0ecf1a9795cf2ae3d47764633b7696c024
Tyler Schloesser <tylerschloesser@gmail.com>
2023-12-28 16:47:49 -0800

    add snapshot

--- packages/demo-gear/src/world-v2.test.ts
+++ packages/demo-gear/src/world-v2.test.ts
@@ -1,9 +1,11 @@
 import { describe, expect, test } from '@jest/globals'
+import { initWorld } from './world-v2.js'
 
 describe('world-v2', () => {
   describe('initWorld', () => {
-    test('todo', () => {
-      expect(false).toBe(false)
+    test('new world', () => {
+      const actual = initWorld()
+      expect(actual).toMatchSnapshot()
     })
   })
 })
