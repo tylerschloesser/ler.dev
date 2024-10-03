89afa64888acfcc22067ac4e4ddce32805a719a2
Tyler Schloesser <tylerschloesser@gmail.com>
2024-10-02 21:13:43 -0700

    fix regions

--- packages/cdk/src/main.ts
+++ packages/cdk/src/main.ts
@@ -4,5 +4,17 @@ import { CertificateStack } from './certificate-stack'
 import { MainStack } from './main-stack'
 
 const app = new cdk.App()
-new MainStack(app, 'LerDev-Main')
-new CertificateStack(app, 'LerDev-Certificate')
+
+new CertificateStack(app, 'LerDev-Certificate', {
+  env: {
+    region: 'us-east-1',
+  },
+  crossRegionReferences: true,
+})
+
+new MainStack(app, 'LerDev-Main', {
+  crossRegionReferences: true,
+  env: {
+    region: 'us-west-2',
+  },
+})
