38fd2d3f227557183bd0c991091efae723b3e5cf
Tyler Schloesser <tylerschloesser@gmail.com>
2024-11-18 13:50:58 -0600

    add canvas

--- packages/app/src/resume-v2/index.tsx
+++ packages/app/src/resume-v2/index.tsx
@@ -1,3 +1,12 @@
+import * as PIXI from 'pixi.js'
+import { useMemo } from 'react'
+import { Vec2 } from './vec2'
+
 export function ResumeV2() {
-  return <>ResumeV2</>
+  const size = useMemo(
+    () => new Vec2(window.innerWidth, window.innerHeight),
+    [],
+  )
+
+  return <canvas width={size.x} height={size.y} />
 }
