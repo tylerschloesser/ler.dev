fc8f83db76c778e21cc90277a58a3943d03057dc
Tyler Schloesser <tylerschloesser@gmail.com>
2024-11-18 17:22:49 -0600

    add noise

--- packages/app/src/resume-v2/index.tsx
+++ packages/app/src/resume-v2/index.tsx
@@ -1,5 +1,7 @@
 import * as PIXI from 'pixi.js'
+import Prando from 'prando'
 import { useEffect, useRef, useState } from 'react'
+import { createNoise3D } from 'simplex-noise'
 import invariant from 'tiny-invariant'
 import { Vec2 } from './vec2'
 
@@ -18,7 +20,11 @@ async function init() {
     height: size.y,
     autoStart: false,
   })
-  return { canvas, app }
+
+  const rng = new Prando(0)
+  const noise = createNoise3D(rng.next.bind(rng))
+
+  return { canvas, app, noise }
 }
 
 type State = Awaited<ReturnType<typeof init>>
@@ -29,26 +35,7 @@ export function ResumeV2() {
   const container = useRef<HTMLDivElement>(null)
   const [state, setState] = useState<State | null>(null)
   useEffect(() => {
-    init().then((_state) => {
-      const rect = new PIXI.Graphics()
-      const size = new Vec2(200)
-      rect.rect(0, 0, size.x, size.y)
-      rect.fill('red')
-      rect.position.set(
-        _state.canvas.width / 2,
-        _state.canvas.height / 2,
-      )
-      rect.pivot.set(size.x / 2, size.y / 2)
-
-      _state.app.stage.addChild(rect)
-      _state.app.start()
-
-      _state.app.ticker.add((ticker) => {
-        rect.rotation += ticker.deltaTime * 1e-2
-      })
-
-      setState(_state)
-    })
+    init().then(setState)
   }, [])
 
   useEffect(() => {
@@ -58,6 +45,31 @@ export function ResumeV2() {
 
     invariant(container.current)
     container.current.append(state.canvas)
+
+    const rect = new PIXI.Graphics()
+    const size = new Vec2(200)
+
+    state.app.stage.addChild(rect)
+    state.app.start()
+
+    state.app.ticker.add((ticker) => {
+      rect.rotation += ticker.deltaTime * 1e-2
+
+      const now = self.performance.now()
+
+      const hue = state.noise(0, 0, now * 1e-5) * 360
+
+      const style = `hsl(${hue.toFixed(2)}, 50%, 50%)`
+      rect.clear()
+
+      rect.rect(0, 0, size.x, size.y)
+      rect.position.set(
+        state.canvas.width / 2,
+        state.canvas.height / 2,
+      )
+      rect.pivot.set(size.x / 2, size.y / 2)
+      rect.fill(style)
+    })
 
     return () => {
       state.app.destroy(
