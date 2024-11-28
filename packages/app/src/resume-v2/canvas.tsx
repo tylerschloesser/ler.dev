8dabd2a5ca362e821295124370c5bc03e06a3b04
Tyler Schloesser <tylerschloesser@gmail.com>
2024-11-27 18:52:20 -0800

    pre canvas state

--- packages/app/src/resume-v2/canvas.tsx
+++ packages/app/src/resume-v2/canvas.tsx
@@ -4,28 +4,36 @@ import invariant from 'tiny-invariant'
 import { Vec2 } from './vec2'
 
 export function Canvas() {
-  const canvas = useRef<HTMLCanvasElement>(null)
+  const container = useRef<HTMLDivElement>(null)
   const interval = useRef<number | null>(null)
   const app = useRef<PIXI.Application | null>(null)
-  const viewport = useViewport(canvas)
+  const viewport = useViewport(container)
   useEffect(() => {
-    if (!canvas.current) return
+    if (!container.current) return
+
+    const canvas = document.createElement('canvas')
+    container.current.appendChild(canvas)
+
     app.current = new PIXI.Application()
-    invariant(canvas.current)
+    invariant(container.current)
     app.current
       .init({
         backgroundAlpha: 0,
-        canvas: canvas.current,
+        canvas,
       })
       .then(() => {
         interval.current = self.setInterval(() => {
-          console.log('hi4')
+          console.log('hi2')
         }, 100)
       })
     return () => {
       if (interval.current) {
         self.clearInterval(interval.current)
       }
+      if (typeof app.current?.destroy === 'function') {
+        app.current.destroy()
+      }
+      canvas.remove()
     }
   }, [])
   useEffect(() => {
@@ -36,18 +44,11 @@ export function Canvas() {
       app.current.resize()
     }
   }, [viewport])
-  return (
-    <canvas
-      ref={canvas}
-      className="w-full h-full"
-      width={viewport?.x}
-      height={viewport?.y}
-    />
-  )
+  return <div ref={container} className="w-full h-full" />
 }
 
 function useViewport(
-  canvas: React.RefObject<HTMLCanvasElement>,
+  container: React.RefObject<HTMLDivElement>,
 ) {
   const [viewport, setViewport] = useState<Vec2 | null>(
     null,
@@ -57,8 +58,8 @@ function useViewport(
       const { contentRect: rect } = entry
       setViewport(new Vec2(rect.width, rect.height))
     })
-    invariant(canvas.current)
-    ro.observe(canvas.current)
+    invariant(container.current)
+    ro.observe(container.current)
     return () => {
       ro.disconnect()
     }
