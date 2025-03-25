0699029264ad03aaca401ba6922131555a98d2e0
Tyler Schloesser <tylerschloesser@gmail.com>
2025-03-25 14:01:07 -0700

    render rect

--- packages/app/src/main.tsx
+++ packages/app/src/main.tsx
@@ -2,6 +2,7 @@ import {
   RouterProvider,
   createRouter,
 } from '@tanstack/react-router'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import invariant from 'tiny-invariant'
 import './index.css'
@@ -19,4 +20,8 @@ const container = document.getElementById('root')
 invariant(container)
 
 const root = ReactDOM.createRoot(container)
-root.render(<RouterProvider router={router} />)
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
--- packages/app/src/routes/index.tsx
+++ packages/app/src/routes/index.tsx
@@ -1,26 +1,65 @@
 import {
   Link,
   createFileRoute,
+  invariant,
 } from '@tanstack/react-router'
+import { Application, Graphics } from 'pixi.js'
+import { useEffect, useRef } from 'react'
 
 export const Route = createFileRoute('/')({
   component: Index,
 })
 
 function Index() {
+  const container = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const canvas = document.createElement('canvas')
+    const app = new Application()
+    invariant(container.current)
+    const promise = app.init({
+      canvas,
+      antialias: true,
+      eventMode: 'none',
+      backgroundAlpha: 0,
+      resizeTo: container.current,
+    })
+    const controller = new AbortController()
+    promise.then(() => {
+      if (controller.signal.aborted) {
+        return
+      }
+      invariant(container.current)
+      container.current.appendChild(canvas)
+
+      const g = new Graphics()
+      app.stage.addChild(g)
+      g.rect(0, 0, 100, 100)
+      g.fill('blue')
+    })
+    return () => {
+      controller.abort()
+      promise.then(() => app.destroy())
+      canvas.remove()
+    }
+  }, [])
+
   return (
-    <>
-      <div className="w-dvw h-dvh flex items-center justify-center">
-        <div className="flex flex-col gap-2">
-          <h1 className="text-center">ty.ler.dev</h1>
-          <Link
-            to="/resume"
-            className="block border p-2 rounded min-w-40 text-center shadow"
-          >
-            Resume
-          </Link>
+    <div className="relative">
+      <div ref={container} className="absolute inset-0" />
+      <div className="relative">
+        <div className="w-dvw h-dvh flex items-center justify-center">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-center">ty.ler.dev</h1>
+            <Link
+              to="/resume"
+              className="block border p-2 rounded min-w-40 text-center shadow"
+            >
+              Resume
+            </Link>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
