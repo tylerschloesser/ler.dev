34fa359283f91acd479a052e82e635602026bf02
Tyler Schloesser <tylerschloesser@gmail.com>
2025-03-26 09:53:52 -0700

    add max width

--- packages/app/src/routes/resume.lazy.tsx
+++ packages/app/src/routes/resume.lazy.tsx
@@ -6,5 +6,20 @@ export const Route = createLazyFileRoute('/resume')({
 })
 
 function RouteComponent() {
-  return <Resume />
+  return (
+    <>
+      <div className="print:hidden">
+        <div className="md:p-8 flex justify-center">
+          <div className="border-2 shadow-xl">
+            <div className="max-w-4xl">
+              <Resume />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="not-print:hidden">
+        <Resume />
+      </div>
+    </>
+  )
 }
