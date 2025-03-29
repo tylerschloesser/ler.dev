78a79784a8dae399146bc58276536b945e5b3fcc
Tyler Schloesser <tylerschloesser@gmail.com>
2025-03-29 09:50:51 -0700

    refactor home background

--- packages/app/src/routes/__root.tsx
+++ packages/app/src/routes/__root.tsx
@@ -3,14 +3,26 @@ import {
   Outlet,
 } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import React, { Suspense } from 'react'
 
 const DEV_MODE = false
 
+const HomeBackground = React.lazy(
+  () => import('../home-background.tsx'),
+)
+
 export const Route = createRootRoute({
   component: () => (
-    <>
-      <Outlet />
+    <div className="relative">
+      <div className="print:hidden absolute w-screen h-screen">
+        <Suspense>
+          <HomeBackground />
+        </Suspense>
+      </div>
+      <div className="relative">
+        <Outlet />
+      </div>
       {DEV_MODE && <TanStackRouterDevtools />}
-    </>
+    </div>
   ),
 })
--- packages/app/src/routes/index.tsx
+++ packages/app/src/routes/index.tsx
@@ -3,11 +3,7 @@ import {
   createFileRoute,
 } from '@tanstack/react-router'
 import clsx from 'clsx'
-import React, { Suspense, useEffect } from 'react'
-
-const HomeBackground = React.lazy(
-  () => import('../home-background.tsx'),
-)
+import { useEffect } from 'react'
 
 export const Route = createFileRoute('/')({
   component: Index,
@@ -16,15 +12,18 @@ export const Route = createFileRoute('/')({
 function Index() {
   useDisableOverscroll()
   return (
-    <div className="relative">
-      <Suspense>
-        <HomeBackground />
-      </Suspense>
+    <>
       <div className="relative">
         <div className="w-dvw h-dvh flex items-center justify-center">
           <div className="flex flex-col gap-2">
             <h1 className="text-center">ty.ler.dev</h1>
             <Link
+              onPointerOver={() => {
+                console.log('over')
+              }}
+              onPointerLeave={() => {
+                console.log('leave')
+              }}
               to="/resume"
               className={clsx(
                 'block border p-2 rounded min-w-40 text-center shadow',
@@ -37,7 +36,7 @@ function Index() {
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
